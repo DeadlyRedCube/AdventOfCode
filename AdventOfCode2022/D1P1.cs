afdb30c9f836768aa370d6ad8419eab434903c02
DeadlyRedCube <josh@drilsej.com>
2022-12-04 13:48:42 -0800

    Day 1 Puzzle 1

---
+++ AdventOfCode2022/D1P1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022
+{
+  internal static class D1P1
+  {
+    public static void Run(string input)
+    {
+      int mostCals = 0;
+      int bestElf = 0;
+
+      int curElf = 1;
+      int curCals = 0;
+      foreach (var striter in (input + "\n").Split('\n'))
+      {
+        var str = striter.Trim();
+        if (str.Length == 0)
+        {
+          if (curCals > mostCals)
+          {
+            mostCals = curCals;
+            bestElf = curElf;
+          }
+
+          curCals = 0;
+          curElf++;
+        }
+        else
+        {
+          curCals += int.Parse(str);
+        }
+      }
+
+      Console.WriteLine($"Best elf: {bestElf} with {mostCals} calories");
+    }
+  }
+}
---
+++ AdventOfCode2022/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AdventOfCode2022;
+
+Console.WriteLine("Hello, World!");
+
+
+D1P1.Run(File.ReadAllText("D1P1.txt"));
+

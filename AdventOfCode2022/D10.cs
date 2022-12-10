2bc9cc644cf693553bf480d2a81f27c66e54dc78
DeadlyRedCube <josh@drilsej.com>
2022-12-09 21:14:34 -0800

    Day 10 Puzzle 1

---
+++ AdventOfCode2022/D10.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022
+{
+  internal static class D10
+  {
+    public static void Run(string input)
+    {
+      int x = 1;
+      int cycleIndex = 0;
+
+      var testCycleIndices = new int[] { 20, 60, 100, 140, 180, 220 };
+
+      int strengthSum = 0;
+
+      foreach (var line in input.Split("\n", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries))
+      {
+        // Run cycle 1 regardless
+        bool add = line.StartsWith("addx");
+
+        for (int i = 0; i < (add ? 2 : 1); i++)
+        {
+          cycleIndex++;
+          if (testCycleIndices.Contains(cycleIndex))
+          {
+            strengthSum += cycleIndex * x;
+            Console.WriteLine($"Cycle {cycleIndex}, Strength: {cycleIndex * x}");
+          }
+        }
+
+        if (add)
+        {
+          x += int.Parse(line.Split(" ", StringSplitOptions.TrimEntries)[1]);
+        }
+      }
+
+      Console.WriteLine($"Signal Sum: {strengthSum}");
+    }
+  }
+}
--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -14,6 +14,7 @@ Console.WriteLine("Hello, World!");
 //D6.RunP2(File.ReadAllText("D6.txt"));
 //D7.Run(File.ReadAllText("D7.txt"));
 //D8.Run(File.ReadAllText("D8.txt"));
-D9.RunP1(File.ReadAllText("D9.txt"));
-D9.RunP2(File.ReadAllText("D9.txt"));
+//D9.RunP1(File.ReadAllText("D9.txt"));
+//D9.RunP2(File.ReadAllText("D9.txt"));
+D10.Run(File.ReadAllText("D10.txt"));
 

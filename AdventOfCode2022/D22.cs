c5cbe0a804f717a0ea9a94dd88b287dc8363cb8d
DeadlyRedCube <josh@drilsej.com>
2022-12-21 23:32:47 -0800

    Day 22 Part 2

--- AdventOfCode2022/D22.cs
+++ AdventOfCode2022/D22.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Numerics;
 using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
@@ -66,6 +67,7 @@ namespace AdventOfCode2022
       }
     }
 
+    [DebuggerDisplay("{X}, {Y}")]
     struct Vec
     {
       public int X;
@@ -76,8 +78,81 @@ namespace AdventOfCode2022
 
       public static Vec operator+(Vec a, Vec b)
         => new Vec{X = a.X + b.X, Y = a.Y + b.Y};
+
+      public static Vec operator-(Vec a, Vec b)
+        => new Vec{X = a.X - b.X, Y = a.Y - b.Y};
     }
 
+
+    [DebuggerDisplay("{X}, {Y}, {Z}")]
+    struct Vec3
+    {
+      public int X;
+      public int Y;
+      public int Z;
+
+      public Vec3(int x, int y, int z)
+        { X = x; Y = y; Z = z; }
+
+      public static Vec3 operator+(Vec3 a, Vec3 b)
+        => new Vec3{X = a.X + b.X, Y = a.Y + b.Y, Z = a.Z + b.Z};
+
+      public static Vec3 operator-(Vec3 a, Vec3 b)
+        => new Vec3{X = a.X - b.X, Y = a.Y - b.Y, Z = a.Z - b.Z};
+
+      public static Vec3 operator*(Vec3 a, int b)
+        => new Vec3(a.X * b, a.Y * b, a.Z * b);
+
+      public static Vec3 operator-(Vec3 a)
+        => new Vec3(-a.X, -a.Y, -a.Z);
+
+      public static Vec3 Cross(Vec3 a, Vec3 b)
+        => new Vec3(a.Y*b.Z - a.Z*b.Y, -(a.X*b.Z - a.Z*b.X), a.X*b.Y - a.Y*b.X);
+
+      public static int Dot(Vec3 a, Vec3 b)
+        => a.X*b.X + a.Y*b.Y + a.Z*b.Z;
+
+      public static Vec3 RotateRight(Vec3 facing, Vec3 axis)
+        => Cross(facing, axis);
+
+      public static Vec3 RotateLeft(Vec3 facing, Vec3 axis)
+        => Cross(axis, facing);
+
+      public int MinComponent { get { return Math.Min(X, Math.Min(Y, Z)); } }
+      public int MaxComponent { get { return Math.Max(X, Math.Max(Y, Z)); } }
+      public int ManhattanLength { get { return Math.Abs(X) + Math.Abs(Y) + Math.Abs(Z); } }
+
+      public static bool operator==(Vec3 a, Vec3 b)
+        => a.X == b.X && a.Y == b.Y && a.Z == b.Z;
+
+      public static bool operator!=(Vec3 a, Vec3 b)
+        => !(a == b);
+    }
+
+    class Face
+    {
+      public Vec mapOrigin;
+      public Vec3 normal;
+      public Vec3 uDir;
+      public Vec3 vDir;
+      public char[,] map;
+
+      public Vec Project(Vec3 pos)
+      {
+        Vec p = new Vec(Vec3.Dot(pos, uDir), Vec3.Dot(pos, vDir));
+        if (uDir.MinComponent < 0)
+        {
+          p.X = map.GetLength(0) - 1 + p.X;
+        }
+
+        if (vDir.MinComponent < 0)
+        {
+          p.Y = map.GetLength(1) - 1 + p.Y;
+        }
+
+        return p;
+      }
+    }
 
     public static void Run(string input)
     {
@@ -118,111 +193,301 @@ namespace AdventOfCode2022
         }
       }
 
-      Vec p = new Vec(0,0);
-      Facing f = Facing.Right;
-
-      while (map[p.X, 0] != '.')
-        { p.X++; }
-
-      var stepDirs = new Vec[] { new Vec(1, 0), new Vec(0, 1), new Vec(-1, 0), new Vec(0, -1) };
       var directions = input.Split(new string[] { "\r\n", "\n"}, StringSplitOptions.TrimEntries).Last();
+      var stepDirs = new Vec[] { new Vec(1, 0), new Vec(0, 1), new Vec(-1, 0), new Vec(0, -1) };
 
-      char[,] stepped = new char[width, height];
-      foreach (var t in Tokenize(directions))
+      // Part 1
       {
-        //Console.WriteLine($"{t}");
-        if (int.TryParse(t, out var stepCount))
+        Vec p = new Vec(0,0);
+        Facing f = Facing.Right;
+
+        while (map[p.X, 0] != '.')
+          { p.X++; }
+
+        char[,] stepped = new char[width, height];
+        foreach (var t in Tokenize(directions))
         {
-          var d = stepDirs[(int)f];
-          for (int i = 0; i < stepCount; i++)
+          if (int.TryParse(t, out var stepCount))
           {
-            var old = p;
-            p += d;
-            if (d.X > 0)
+            var d = stepDirs[(int)f];
+            for (int i = 0; i < stepCount; i++)
             {
-              if (p.X >= width || map[p.X, p.Y] == ' ')
+              var old = p;
+              p += d;
+              if (d.X > 0)
               {
-                while (p.X > 0 && map[p.X - 1, p.Y] != ' ')
+                if (p.X >= width || map[p.X, p.Y] == ' ')
                 {
-                  p.X--; 
+                  while (p.X > 0 && map[p.X - 1, p.Y] != ' ')
+                  {
+                    p.X--; 
+                  }
                 }
               }
-            }
-            else if (d.X < 0)
-            {
-              if (p.X < 0 || map[p.X, p.Y] == ' ')
+              else if (d.X < 0)
               {
-                while (p.X < width - 1 && map[p.X + 1, p.Y] != ' ')
+                if (p.X < 0 || map[p.X, p.Y] == ' ')
                 {
-                  p.X++; 
+                  while (p.X < width - 1 && map[p.X + 1, p.Y] != ' ')
+                  {
+                    p.X++; 
+                  }
                 }
               }
-            }
-            else if (d.Y > 0)
-            {
-              if (p.Y >= height || map[p.X, p.Y] == ' ')
+              else if (d.Y > 0)
               {
-                while (p.Y > 0 && map[p.X, p.Y - 1] != ' ')
+                if (p.Y >= height || map[p.X, p.Y] == ' ')
                 {
-                  p.Y--; 
+                  while (p.Y > 0 && map[p.X, p.Y - 1] != ' ')
+                  {
+                    p.Y--; 
+                  }
                 }
               }
-            }
-            else
-            {
-              if (p.Y < 0 || map[p.X, p.Y] == ' ')
+              else
               {
-                while (p.Y < height - 1 && map[p.X, p.Y + 1] != ' ')
+                if (p.Y < 0 || map[p.X, p.Y] == ' ')
                 {
-                  p.Y++; 
+                  while (p.Y < height - 1 && map[p.X, p.Y + 1] != ' ')
+                  {
+                    p.Y++; 
+                  }
                 }
               }
-            }
 
-            if (map[p.X, p.Y] == '#')
-            {
-              p = old;
-            }
+              if (map[p.X, p.Y] == '#')
+              {
+                p = old;
+              }
 
-            stepped[p.X, p.Y] = '@';
-          }
-
-          //Console.WriteLine($"  New X: {p.X}, Y: {p.Y}");
-        }
-        else
-        {
-          Debug.Assert(t == "L" || t == "R");
-          f = (t == "L") ? SpinLeft(f) : SpinRight(f);
-          //Console.WriteLine($"New Facing: {f}");
-        }
-
-        #if false
-        for (int y = 0; y < height; y++)
-        {
-          for (int x = 0; x < width; x++)
-          {
-            if (p.X == x && p.Y == y)
-            {
-              Console.Write('@');
-            }
-            else if (stepped[x,y] != 0)
-            {
-              Debug.Assert(map[x,y] == '.');
-              Console.Write(map[x,y] == '.' ? '+' : 'X');
-            }
-            else
-            {
-              Console.Write(map[x,y]);
+              stepped[p.X, p.Y] = '@';
             }
           }
-          Console.WriteLine();
+          else
+          {
+            Debug.Assert(t == "L" || t == "R");
+            f = (t == "L") ? SpinLeft(f) : SpinRight(f);
+          }
         }
 
-        Console.WriteLine($"X: {p.X}, Y: {p.Y}, facing: {f}, move: {t}\n\n");
-        #endif
+        Console.WriteLine($"[p1] Score: {(p.Y + 1) * 1000 + 4 * (p.X + 1) + (int)f}");
       }
 
-      Console.WriteLine($"Score: {(p.Y + 1) * 1000 + 4 * (p.X + 1) + (int)f}");
+      {
+        // Part 2!
+
+        // Figure out how to unwrap the cube
+        // Find the minimum contiguous dimension 
+        int cubeSize = int.MaxValue;
+        for (int x = 0; x < width; x++)
+        {
+          int counter = 0;
+          for (int y = 0; y < height; y++)
+          {
+            if (map[x, y] == ' ')
+            {
+              if (counter > 0)
+              {
+                cubeSize = Math.Min(cubeSize, counter);
+              }
+              counter = 0;
+            }
+            else
+            {
+              counter++;
+            }
+          }
+        }
+
+        for (int y = 0; y < height; y++)
+        {
+          int counter = 0;
+          for (int x = 0; x < width; x++)
+          {
+            if (map[x, y] == ' ')
+            {
+              if (counter > 0)
+              {
+                cubeSize = Math.Min(cubeSize, counter);
+              }
+              counter = 0;
+            }
+            else
+            {
+              counter++;
+            }
+          }
+        }
+
+        Dictionary<Vec, Face> facemap = new Dictionary<Vec, Face>();
+        for (int fy = 0; fy < height; fy += cubeSize)
+        {
+          for (int fx = 0; fx < width; fx += cubeSize)
+          {
+            if (map[fx, fy] == ' ')
+            {
+              continue;
+            }
+
+            var face = new Face();
+            face.mapOrigin = new Vec(fx, fy);
+            face.map = new char[cubeSize, cubeSize];
+            // Oh shit a face here we go
+            for (int x = 0; x < cubeSize; x++)
+            {
+              for (int y = 0; y < cubeSize; y++)
+              {
+                face.map[x,y] = map[x + fx, y + fy];
+              }
+            }
+
+            facemap.Add(new Vec(fx, fy), face);
+          }
+        }
+
+        // Now figure out how to connect them
+        {
+          Queue<Face> q = new Queue<Face>();
+          var firstFace = facemap.First().Value;
+          firstFace.uDir = new Vec3(1, 0, 0);
+          firstFace.vDir = new Vec3(0, 1, 0);
+          firstFace.normal = new Vec3(0, 0, -1);
+          q.Enqueue(firstFace);
+          while (q.Count > 0)
+          {
+            var face = q.Dequeue();
+
+            if (facemap.TryGetValue(new Vec(face.mapOrigin.X + cubeSize, face.mapOrigin.Y), out var rightFace) 
+              && rightFace.normal.ManhattanLength == 0)
+            {
+              // Face is off to the right
+              rightFace.normal = face.uDir;
+              rightFace.vDir = face.vDir;
+              rightFace.uDir = -face.normal;
+              q.Enqueue(rightFace);
+            }
+
+            if (facemap.TryGetValue(new Vec(face.mapOrigin.X - cubeSize, face.mapOrigin.Y), out var leftFace) 
+              && leftFace.normal.ManhattanLength == 0)
+            {
+              // Face is off to the right
+              leftFace.normal = -face.uDir;
+              leftFace.vDir = face.vDir;
+              leftFace.uDir = face.normal;
+              q.Enqueue(leftFace);
+            }
+
+            if (facemap.TryGetValue(new Vec(face.mapOrigin.X, face.mapOrigin.Y + cubeSize), out var downFace) 
+              && downFace.normal.ManhattanLength == 0)
+            {
+              // Face is off to the right
+              downFace.normal = face.vDir;
+              downFace.uDir = face.uDir;
+              downFace.vDir = -face.normal;
+              q.Enqueue(downFace);
+            }
+
+            if (facemap.TryGetValue(new Vec(face.mapOrigin.X, face.mapOrigin.Y - cubeSize), out var upFace) 
+              && upFace.normal.ManhattanLength == 0)
+            {
+              // Face is off to the right
+              upFace.normal = -face.vDir;
+              upFace.uDir = face.uDir;
+              upFace.vDir = face.normal;
+              q.Enqueue(upFace);
+            }
+          }
+        }
+
+        Face curFace;
+        Vec3 curPos;
+        Vec3 facing;
+        {
+          Vec p = new Vec(0,0);
+          while (map[p.X, 0] != '.')
+            { p.X++; }
+
+          var startCoord = new Vec(p.X / cubeSize * cubeSize, p.Y / cubeSize * cubeSize);
+          curFace = facemap[startCoord];
+          Debug.Assert(curFace != null);
+          p -= startCoord;
+          curPos = curFace.uDir * p.X + curFace.vDir * p.Y;
+          facing = curFace.uDir;
+        }
+
+        // Great now we can finally start walking around this thing
+        foreach (var t in Tokenize(directions))
+        {
+          char[,] stepped = new char[width, height];
+          if (int.TryParse(t, out var stepCount))
+          {
+            for (int i = 0; i < stepCount; i++)
+            {
+              var oldPos = curPos;
+              var oldFace = curFace;
+              var oldFacing = facing;
+              curPos += facing;
+              
+              var proj = curFace.Project(curPos);
+              if (proj.X < 0)
+              {
+                // Went off the left
+                curPos = oldPos;
+                facing = -curFace.normal;
+                curFace = facemap.Values.Where(f => f.normal == -curFace.uDir).First();
+              }
+              else if (proj.Y < 0)
+              {
+                // Went off the top
+                curPos = oldPos;
+                facing = -curFace.normal;
+                curFace = facemap.Values.Where(f => f.normal == -curFace.vDir).First();
+              }
+              else if (proj.X >= cubeSize)
+              {
+                // Went off the right
+                curPos = oldPos;
+                facing = -curFace.normal;
+                curFace = facemap.Values.Where(f => f.normal == curFace.uDir).First();
+              }
+              else if (proj.Y >= cubeSize)
+              {
+                // Off bottom
+                curPos = oldPos;
+                facing = -curFace.normal;
+                curFace = facemap.Values.Where(f => f.normal == curFace.vDir).First();
+              }
+            
+              proj = curFace.Project(curPos);
+              if (curFace.map[proj.X, proj.Y] != '.')
+              {
+                Debug.Assert(curFace.map[proj.X, proj.Y] == '#');
+                curPos = oldPos;
+                facing = oldFacing;
+                curFace = oldFace;
+              }
+              else
+              {
+                stepped[proj.X + curFace.mapOrigin.X, proj.Y + curFace.mapOrigin.Y] = '@';
+              }
+            }
+          }
+          else
+          {
+            Debug.Assert(t == "L" || t == "R");
+            facing = (t == "L") ? Vec3.RotateLeft(facing, curFace.normal) : Vec3.RotateRight(facing, curFace.normal);
+          }
+        }
+
+        {
+          var proj = curFace.Project(curPos);
+          var dir = curFace.Project(curPos + facing) - proj;
+          proj += curFace.mapOrigin;
+          int f =  stepDirs.ToList().IndexOf(dir);
+
+          Console.WriteLine($"[p2] Score: {(proj.Y + 1) * 1000 + 4 * (proj.X + 1) + f}");
+        }
+      }
     }
   }
 }

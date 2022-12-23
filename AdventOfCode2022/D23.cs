0cc59f9b4f3d2e91975aeed7274a5acee3e1e40b
DeadlyRedCube <josh@drilsej.com>
2022-12-23 02:37:11 -0800

    Day 23: some simplification, some very questionable LINQ choices

--- AdventOfCode2022/D23.cs
+++ AdventOfCode2022/D23.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Reflection.Metadata.Ecma335;
-using System.Security.Principal;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Net.Mime.MediaTypeNames;
+﻿using System.Diagnostics;
 
 namespace AdventOfCode2022
 {
@@ -32,14 +24,17 @@ namespace AdventOfCode2022
 
       public static Vec Max(Vec a, Vec b)
         => new Vec(Math.Max(a.X, b.X), Math.Max(a.Y, b.Y));
+
+      public static bool operator==(Vec a, Vec b)
+        => a.X == b.X && a.Y == b.Y;
+
+      public static bool operator!=(Vec a, Vec b)
+        => !(a == b);
     }
 
-
-    struct Move
+    class Move
     {
-      public Vec testA;
-      public Vec testB;
-      public Vec testC;
+      public Vec[] tests;
       public Vec target;
     }
 
@@ -47,137 +42,73 @@ namespace AdventOfCode2022
     {
       var moves = new Move[]
       {
-        new Move
-        {
-          testA  = new Vec(-1, -1),
-          testB  = new Vec( 0, -1),
-          testC  = new Vec( 1, -1),
-          target = new Vec( 0, -1),
-        },
-        new Move
-        {
-          testA  = new Vec(-1,  1),
-          testB  = new Vec( 0,  1),
-          testC  = new Vec( 1,  1),
-          target = new Vec( 0,  1),
-        },
-        new Move
-        {
-          testA  = new Vec(-1, -1),
-          testB  = new Vec(-1,  0),
-          testC  = new Vec(-1,  1),
-          target = new Vec(-1,  0),
-        },
-        new Move
-        {
-          testA  = new Vec( 1, -1),
-          testB  = new Vec( 1,  0),
-          testC  = new Vec( 1,  1),
-          target = new Vec( 1,  0),
-        },
-      }.ToList();
+        new Move { tests = new Vec[] { new Vec(-1, -1), new Vec( 0, -1), new Vec( 1, -1) }, target = new Vec( 0, -1), },
+        new Move { tests = new Vec[] { new Vec(-1,  1), new Vec( 0,  1), new Vec( 1,  1) }, target = new Vec( 0,  1), },
+        new Move { tests = new Vec[] { new Vec(-1, -1), new Vec(-1,  0), new Vec(-1,  1) }, target = new Vec(-1,  0), },
+        new Move { tests = new Vec[] { new Vec( 1, -1), new Vec( 1,  0), new Vec( 1,  1) }, target = new Vec( 1,  0), },
+      }.AsReadOnly();
 
-      var ring = new Vec[]
-      {
-        new Vec(-1, -1),
-        new Vec( 0, -1),
-        new Vec( 1, -1),
-        new Vec( 1,  0),
-        new Vec( 1,  1),
-        new Vec( 0,  1),
-        new Vec(-1,  1),
-        new Vec(-1,  0),
-      };
+      int initialMoveIndex = 0;
+
+      // get the x, y coordinates for all 8 directions (centered around 0, 0)
+      //  (yeah this is a nightmarish assembly but I'm trying to push LINQ a bit and it was less LOC so here we gooooooo)
+      var ring = Enumerable.Range(-1, 3)
+        .Select(y => Enumerable.Range(-1, 3).Select(x => new Vec(x, y)))
+        .Aggregate(Enumerable.Empty<Vec>(), Enumerable.Concat)
+        .Where(v => v != new Vec(0, 0))
+        .ToArray().AsReadOnly();
       
-      var elves = new HashSet<Vec>();
-      {
-        var lines = input.Split('\n', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
-        for (int y = 0; y < lines.Length; y++)
-        {
-          for (int x = 0; x < lines[y].Length; x++)
-          {
-            if (lines[y][x] == '#')
-            {
-              elves.Add(new Vec(x + 1, y + 1));
-            }
-          }
-        }
-      }
+      // Convert all of the '#' marks in the input into elf coordinates
+      //  (ditto the above nightmarishness)
+      var elves = input
+        .Split('\n')
+        .Select((line, y) => line.Select((ch, x) => (ch, x)).Where(v => v.ch == '#').Select(v => new Vec(v.x, y)))
+        .Aggregate(Enumerable.Empty<Vec>(), Enumerable.Concat)
+        .ToHashSet();
 
-      for (int round = 1;; round++)
+      for (int round = 1;; round++, initialMoveIndex++)
       {
-        // Step 1: scan
+        // Step 1: scan for moves
         var elfMoves = new Dictionary<Vec, Vec>();
         var destinations = new Dictionary<Vec, int>();
+
         foreach (var elf in elves)
         {
-          bool wantsToMove = ring.Select(d => elf + d).Any(elves.Contains);
-          if (wantsToMove)
+          // An elf only wants to move if it's near any other elves
+          if (ring.Select(d => elf + d).Any(elves.Contains))
           {
-            foreach (var move in moves)
+            // Iterate through our moves, rotated via our initial move index
+            foreach (var move in Enumerable.Range(initialMoveIndex, 4).Select(i => moves[i % 4]))
             {
-              if ( !elves.Contains(elf + move.testA)
-                && !elves.Contains(elf + move.testB)
-                && !elves.Contains(elf + move.testC))
+              // If none of the test directions contain an elf, we'll move this way
+              if (!move.tests.Select(d => elf + d).Any(elves.Contains))
               {
-                var dest = elf + move.target;
-                elfMoves.Add(elf, dest);
-                destinations.TryAdd(dest, 0);
-                destinations[dest]++;
+                // We'll move!
+                elfMoves.Add(elf, elf + move.target);
+                destinations.TryAdd(elfMoves[elf], 0);
+                destinations[elfMoves[elf]]++;
                 break;
               }
             }
           }
         }
 
-        if (round == 10)
-        {
-          var min = elves.Aggregate(elves.First(), (m, e) => Vec.Min(m, e));
-          var max = elves.Aggregate(elves.First(), (m, e) => Vec.Max(m, e));
-          int emptyCount = (max.X - min.X + 1) * (max.Y - min.Y + 1) - elves.Count;
-          Console.WriteLine($"[P1] Empty Count: {emptyCount}");
-        }
-
         if (elfMoves.Count == 0)
         {
+          // Nobody wanted to move so we're done!
           Console.WriteLine($"[P2] Round: {round}");
           break;
         }
 
-        var newElves = new HashSet<Vec>();
-        foreach (var elf in elves)
+        // Update all of the elves in the set (moving them if they have a move) by creating a new hash set
+        elves = elves.Select(e => (elfMoves.ContainsKey(e) && destinations[elfMoves[e]] == 1) ? elfMoves[e] : e).ToHashSet();
+
+        if (round == 10)
         {
-          if (elfMoves.ContainsKey(elf) && destinations[elfMoves[elf]] == 1)
-          {
-            newElves.Add(elfMoves[elf]);
-          }
-          else
-          {
-            newElves.Add(elf);
-          }
+          var min = elves.Aggregate(elves.First(), Vec.Min);
+          var max = elves.Aggregate(elves.First(), Vec.Max);
+          Console.WriteLine($"[P1] Empty Count: {(max.X - min.X + 1) * (max.Y - min.Y + 1) - elves.Count}");
         }
-
-        elves = newElves;
-        var cycled = moves[0];
-        moves.RemoveAt(0);
-        moves.Add(cycled);
-
-        #if false
-        {
-          var min = elves.Aggregate(elves.First(), (m, e) => Vec.Min(m, e));
-          var max = elves.Aggregate(elves.First(), (m, e) => Vec.Max(m, e));
-
-          Console.WriteLine("\n");
-          for (int y = min.Y; y <= max.Y; y++)
-          {
-            for (int x = min.X; x <= max.X; x++)
-            {
-              Console.Write(elves.Contains(new Vec(x, y)) ? "#" : ".");
-            }
-            Console.WriteLine();
-          }
-        }
-        #endif
       }
     }
   }

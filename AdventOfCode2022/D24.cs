6967267877b80d891d7950b2fc13985460e18d70
DeadlyRedCube <josh@drilsej.com>
2022-12-24 01:10:47 -0800

    Day 24: some additional cleanup
    Don't need my absolutely terrible first attempt in there anymore

--- AdventOfCode2022/D24.cs
+++ AdventOfCode2022/D24.cs
@@ -58,7 +58,6 @@ namespace AdventOfCode2022
       Right,
     }
 
-
     struct Object
     {
       public Vec pos;
@@ -74,68 +73,8 @@ namespace AdventOfCode2022
     static readonly Vec[] PotentialMoves = new Vec[]
       { new Vec(1, 0), new Vec(0, 1), new Vec(0, 0), new Vec(0, -1), new Vec(-1, 0) };
 
-    static readonly int[] PotentialMoveFlags = new int[]
-      { 1 << (int)Type.Right, 1 << (int)Type.Down, 1, 1 << (int)Type.Left, 1 << (int)Type.Up };
-
     static readonly int[] HorzMoveAxes = new int[] { 0, 1, 2, 3, 4 };
     static readonly int[] VertMoveAxes = new int[] { 1, 0, 2, 4, 3 };
-
-    #if false
-    static void MinSteps(long roundIndex, Vec exp, Dictionary<long, Blizzards> blizzards, long[,] shortestWalks)
-    {
-      var bounds = new Vec(shortestWalks.GetLength(0), shortestWalks.GetLength(1));
-      var target = bounds + new Vec(-1, -1);
-
-      if(exp == target)
-      {
-        // Terminal case, we reached the end
-        return;
-      }
-
-      // Move all the blizzards 
-      Blizzards newBlizzards;
-      if (!blizzards.TryGetValue(roundIndex, out newBlizzards))
-      {
-        newBlizzards = new Blizzards();
-        newBlizzards.blizzards = new List<Object>();
-        blizzards.Add(roundIndex, newBlizzards);
-
-        foreach (var b in blizzards[roundIndex - 1].blizzards)
-        {
-          var newPos = b.type switch
-          {
-            Type.Up    => Vec.WrapTo(b.pos + new Vec( 0, -1), bounds),
-            Type.Down  => Vec.WrapTo(b.pos + new Vec( 0,  1), bounds),
-            Type.Left  => Vec.WrapTo(b.pos + new Vec(-1,  0), bounds),
-            Type.Right => Vec.WrapTo(b.pos + new Vec( 1,  0), bounds),
-            _ => throw new InvalidOperationException(),
-          };
-
-          newBlizzards.blizzards.Add(new Object { pos = newPos, type = b.type });
-        }
-
-        newBlizzards.positions = newBlizzards.blizzards.Select(v => v.pos).ToHashSet();
-      }
-
-      // Prioritize whichever direction takes us along the farther axis first
-      var delta = Vec.Abs(target - exp);
-
-      foreach (var i in (delta.X > delta.Y) ? HorzMoveAxes : VertMoveAxes)
-      {
-        var t = exp + PotentialMoves[i];
-        if (t.X < 0 || t.Y < 0 || t.X >= bounds.X || t.Y >= bounds.Y)
-          { continue; }
-
-        if (newBlizzards.positions.Contains(t))
-          { continue; }
-
-        if (shortestWalks[t.X, t.Y] > 
-        bestSteps = Math.Min(bestSteps, MinSteps(roundIndex + 1, t, blizzards, bounds, bestSteps));
-      }
-
-      return bestSteps;
-    }
-    #endif
 
     static int Walk(int startStep, Vec start, Vec target, List<Blizzards> blizzards, Vec bounds)
     {
@@ -219,6 +158,7 @@ namespace AdventOfCode2022
       int height = lines.Length - 2;
       int width = lines[0].Length - 2;
 
+      // Grab all the blizzard positions into an IEnumerable
       var blizzardStarts = lines
         .Select((line, y) => line
           .Select((ch, x) => (ch, x))
@@ -236,13 +176,15 @@ namespace AdventOfCode2022
               },
             }))
           .Aggregate(Enumerable.Empty<Object>(), Enumerable.Concat);
-      var expStartPos = new Vec(0, -1);
-      var target = new Vec(width - 1, height);
 
       var blizzards = new List<Blizzards>();
       blizzards.Add(new Blizzards { blizzards = blizzardStarts.ToList(), positions = blizzardStarts.Select(v => v.pos).ToHashSet() });
       
       var bounds = new Vec(width, height);
+
+      // We start off the top on the left and end off the bottom on the right
+      var expStartPos = new Vec(0, -1);
+      var target = new Vec(width - 1, height);
 
       var best = Walk(1, expStartPos, target, blizzards, bounds);
       Console.WriteLine($"[P1] Best: {best}");

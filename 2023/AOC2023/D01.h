49111358cdd3d16fdc903cdcdaefa55434e5a10b
DeadlyRedCube <josh@drilsej.com>
2023-12-02 19:40:33 -0800

    2023 Day 1 Part 2

--- 2023/AOC2023/D01.h
+++ 2023/AOC2023/D01.h
@@ -1,16 +1,89 @@
 #pragma once
 
 
+int D01FindFirstDigitOrName(const std::string &s)
+{
+  std::string sub = s;
+  while (sub.length() > 0)
+  {
+    if (sub.starts_with("zero"))
+      { return 0; }
+    if (sub.starts_with("one"))
+      { return 1; }
+    if (sub.starts_with("two"))
+      { return 2; }
+    if (sub.starts_with("three"))
+      { return 3; }
+    if (sub.starts_with("four"))
+      { return 4; }
+    if (sub.starts_with("five"))
+      { return 5; }
+    if (sub.starts_with("six"))
+      { return 6; }
+    if (sub.starts_with("seven"))
+      { return 7; }
+    if (sub.starts_with("eight"))
+      { return 8; }
+    if (sub.starts_with("nine"))
+      { return 9; }
+    if (std::isdigit(sub[0]))
+      { return sub[0] - '0'; }
+
+    sub = sub.substr(1);
+  }
+
+  assert(false);
+  return -1;
+}
+
+
+int D01FindLastDigitOrName(const std::string &s)
+{
+  auto rev = s;
+  std::ranges::reverse(rev);
+  std::string sub = rev;
+  while (sub.length() > 0)
+  {
+    if (sub.starts_with("orez"))
+      { return 0; }
+    if (sub.starts_with("eno"))
+      { return 1; }
+    if (sub.starts_with("owt"))
+      { return 2; }
+    if (sub.starts_with("eerht"))
+      { return 3; }
+    if (sub.starts_with("ruof"))
+      { return 4; }
+    if (sub.starts_with("evif"))
+      { return 5; }
+    if (sub.starts_with("xis"))
+      { return 6; }
+    if (sub.starts_with("neves"))
+      { return 7; }
+    if (sub.starts_with("thgie"))
+      { return 8; }
+    if (sub.starts_with("enin"))
+      { return 9; }
+    if (std::isdigit(sub[0]))
+      { return sub[0] - '0'; }
+
+    sub = sub.substr(1);
+  }
+
+  assert(false);
+  return -1;
+}
+
 void D01(const char *path)
 {
   auto lines = ReadFileLines(path);
   int sum = 0;
   for (auto line : lines)
   {
-    auto first = std::ranges::find_if(line, [](auto c){ return std::isdigit(c); });
-    auto last = std::ranges::find_if(std::views::reverse(line), [](auto c){ return std::isdigit(c); });
+    auto firstDig = D01FindFirstDigitOrName(line);
+    auto lastDig = D01FindLastDigitOrName(line);
 
-    int num = (*first - '0') * 10 + (*last - '0');
+    int num = firstDig * 10 + lastDig;
     printf(std::format("{}\n", num).c_str());
     sum += num;
   }

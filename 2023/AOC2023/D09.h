6e42df892126ca10e0a0e4cedccb18301fa08912
DeadlyRedCube <josh@drilsej.com>
2023-12-08 21:21:33 -0800

    2023 Day 9 Part 2

--- 2023/AOC2023/D09.h
+++ 2023/AOC2023/D09.h
@@ -7,6 +7,7 @@ namespace D09
     auto lines = ReadFileLines(path);
 
     ssz sum = 0;
+    s64 sump2 = 0;
     for (auto line : lines)
     {
       auto nums = Split(line, " ", KeepEmpty::No) | std::views::transform([](auto &&s) { return s64(std::atoi(s.c_str())); }) | std::ranges::to<std::vector>();
@@ -37,8 +38,19 @@ namespace D09
 
       PrintFmt("Last: {}\n", histories[0][FromEnd(-1)]);
       sum += histories[0][FromEnd(-1)];
+
+      histories[FromEnd(-1)].Insert(0, 0);
+
+      for (ssz i = histories.Count() - 2; i >= 0; i--)
+      {
+        histories[i].Insert(0, histories[i][0] - histories[i + 1][0]);
+      }
+
+      PrintFmt("LastP2: {}\n", histories[0][0]);
+      sump2 += histories[0][0];
     }
 
-    PrintFmt("Sum: {}\n", sum);
+    PrintFmt("Part 1: {}\n", sum);
+    PrintFmt("Part 2: {}\n", sump2);
   }
 }

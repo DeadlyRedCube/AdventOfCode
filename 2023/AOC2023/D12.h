8c842a856755ca6dc34788377ee11d0a2cd14329
DeadlyRedCube <josh@drilsej.com>
2023-12-13 16:43:24 -0800

    2023 Day 12 simplification
    Realized I did way too much work on this one (I made it so much harder than I needed to)
    
    It now goes like this:
    
    - If we're out of counts to match, this is a match if there are no remaining '#' characters, otherwise it is not
    - After that, skip any leading '.' characters since we cannot match them
    - Bounce out if the length of the string is less than the minimum we'd need to match the rest of our count values
    - Now, we try to treat the starting point as a '#' (it might be a '?'), see if we can match here (with space after to hold a '.' if we have more values)
    - If so, increment our match count
    - Finally, if the starting point is a '?' we also recurse, skipping the first character (treating it like it was a '.') and adding the match count from there
    
    Plus, of course, the caching at each string length/broken count combo.
    
    Now it's about twice as fast (and it was already pretty fast!)

--- 2023/AOC2023/D12.h
+++ 2023/AOC2023/D12.h
@@ -14,106 +14,52 @@ namespace D12
   };
 
 
-  s64 MatchCount(std::string str, ArrayView<s64> brokenCounts, std::map<CacheKey, s64> &cache)
+  s64 MatchCount(std::string_view str, ArrayView<s64> brokenCounts, std::map<CacheKey, s64> &cache)
   {
-    // Skip any leading dots (they cannot somehow become relevant to later matches)
-    while (!str.empty() && str[0] == '.')
-      { str = str.substr(1); }
-
-    // If we have already seen this configuration of string segment and broken elements, we can return our cached
-    // value.
     if (auto d = cache.find({ .stringLength = s64(str.length()), .brokenCounts = brokenCounts.Count() });
       d != cache.end())
       { return d->second; }
 
     if (brokenCounts.IsEmpty())
     {
-      if (std::ranges::any_of(str, [](char c) { return c == '#'; }))
-      {
-        // We have no more items to match, however there are still unconsumed '#'s in the string, so this is not a
-        //  match.
-        cache[{ .stringLength = s64(str.length()), .brokenCounts = brokenCounts.Count() }] = 0;
-        return 0;
-      }
-
-      // We found one (1) match! Yay.
-      cache[{ .stringLength = s64(str.length()), .brokenCounts = brokenCounts.Count() }] = 1;
-      return 1;
-    }
-
-    {
-      s64 necessaryCount = 0;
-      for (auto &c : brokenCounts)
-        { necessaryCount += c; }
-      necessaryCount += brokenCounts.Count() - 1;
-
-      if (necessaryCount > str.length())
-      {
-        // If we have more stuff to match than string length, we can't.
-        cache[{ .stringLength = s64(str.length()), .brokenCounts = brokenCounts.Count() }] = 0;
-        return 0;
-      }
-    }
-
-    {
-      // Check to see if we hit any '.'s before we hit enough '#' or '?' entries to fulfil this match.
-      bool anyHash = false;
-      for (s64 i = 0; i < brokenCounts[0]; i++)
-      {
-        if (str[i] == '#')
-          { anyHash = true; }
-
-        if (str[i] == '.')
-        {
-          if (anyHash)
-          {
-            // Unfortunately, we can't match here but there are '#'s that would need to be matched
-            cache[{ .stringLength = s64(str.length()), .brokenCounts = brokenCounts.Count() }] = 0;
-            return 0;
-          }
-
-          // can't match here so try to match after this point
-          auto r = MatchCount(str.substr(i + 1), brokenCounts, cache);
-          cache[{ .stringLength = s64(str.length()), .brokenCounts = brokenCounts.Count() }] = r;
-          return r;
-        }
-      }
-    }
-
-    // We can maybe match here, figure out how to arrange it
-    if (str[brokenCounts[0]] == '#')
-    {
-      // Can't match here, if we aren't at a '#' try matching again one char over.
-      s64 r = (str[0] == '#') ? 0 : MatchCount(str.substr(1), brokenCounts, cache);
+      // If we're out of things to match, this counts as a match unless there are remaining '#'es
+      s64 r = std::ranges::any_of(str, [](char c) { return c == '#'; }) ? 0 : 1;
       cache[{ .stringLength = s64(str.length()), .brokenCounts = brokenCounts.Count() }] = r;
       return r;
     }
 
-    if(str[0] == '#')
+    // Skip any leading '.'s, they're irrelevant
+    while (!str.empty() && str[0] == '.')
+      { str = str.substr(1); }
+
+    if (str.length() < usz(AccumulateRange(brokenCounts, s64(0)) + brokenCounts.Count() - 1))
     {
-      // We have to match right here, so register the match, and move on to the next area, skipping one additional
-      //  character (unless we're at end of string) which must be either a '?' or a '.' (which we'll treat as a '.'
-      //  either way)
-      auto r = MatchCount(
-        str.substr(std::min(s64(str.length()), brokenCounts[0] + 1)),
-        { brokenCounts, 1, ToEnd },
+      // Not enough string length left to match this entry so we're done.
+      cache[{ .stringLength = s64(str.length()), .brokenCounts = brokenCounts.Count() }] = 0;
+      return 0;
+    }
+
+    s64 matches = 0;
+
+    // Treat the starting point as a '#' - if we have a continuous string of ? or # to match the current broken entry,
+    //  and we're either at the end of the list or the next element can be treated as a '.', we can match here.
+    if (std::ranges::all_of(str.substr(0, usz(brokenCounts[0])), [](char c) { return c != '.'; })
+      && (brokenCounts.Count() == 1 || (str.length() > usz(brokenCounts[0]) && str[brokenCounts[0]] != '#')))
+    {
+      matches += MatchCount(
+        str.substr(brokenCounts[0] + ((brokenCounts.Count() == 1) ? 0 : 1)), // Skip the matched portion
+        { brokenCounts, 1, ToEnd }, // Move on to the next entry in the list
         cache);
-      cache[{ .stringLength = s64(str.length()), .brokenCounts = brokenCounts.Count() }] = r;
-      return r;
     }
 
+    if (str[0] == '?')
+    {
+      // Can also try to treat this as a '.' so skip this character and try matching again at the next spot.
+      matches += MatchCount(str.substr(1), brokenCounts, cache);
+    }
 
-    // Question mark, so we CAN match here. Start by counting the matches that happen if we match our current entry
-    //  right here.
-    s64 count = MatchCount(
-      str.substr(std::min(s64(str.length()), brokenCounts[0] + 1)),
-      { brokenCounts, 1, ToEnd },
-      cache);
-
-    // We can also treat this ? as a ., so skip it and keep trying matches
-    auto r = count + MatchCount(str.substr(1), brokenCounts, cache);
-    cache[{ .stringLength = s64(str.length()), .brokenCounts = brokenCounts.Count() }] = r;
-    return r;
+    cache[{ .stringLength = s64(str.length()), .brokenCounts = brokenCounts.Count() }] = matches;
+    return matches;
   }
 
 
@@ -128,21 +74,16 @@ namespace D12
       auto splits = Split(line, " ,", KeepEmpty::No);
 
       UnboundedArray<s64> brokenCounts;
-      brokenCounts.AppendMultiple(
-        splits | std::views::drop(1) | std::views::transform([](auto &&s) { return std::atoll(s.c_str()); }) | std::ranges::to<std::vector>());
+      brokenCounts.AppendMultiple(splits | std::views::drop(1) | std::views::transform(StrToNum));
 
       std::map<CacheKey, s64> cache;
       auto count = MatchCount(splits[0], brokenCounts, cache);
       p1 += count;
 
-      auto p2Str = splits[0];
-      UnboundedArray<s64> p2BrokenCounts = brokenCounts;
-      for (auto i = 0; i < 4; i++)
-      {
-        p2Str += '?';
-        p2Str += splits[0];
-        p2BrokenCounts.AppendMultiple(brokenCounts);
-      }
+      auto p2Str  = Join({ splits[0], splits[0], splits[0], splits[0], splits[0] }, "?");
+      UnboundedArray<s64> p2BrokenCounts;
+      for (auto i = 0; i < 5; i++)
+        { p2BrokenCounts.AppendMultiple(brokenCounts); }
 
       std::map<CacheKey, s64> p2Cache;
       count = MatchCount(p2Str, p2BrokenCounts, p2Cache);

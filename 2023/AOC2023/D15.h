d71fc152088f3e170762d1a8a7c2280590db2f96
DeadlyRedCube <josh@drilsej.com>
2023-12-15 02:33:18 -0800

    2023 Day 15 Part 2

--- 2023/AOC2023/D15.h
+++ 2023/AOC2023/D15.h
@@ -25,6 +25,55 @@ namespace D15
       sum += Hash(split);
     }
 
-    PrintFmt("{}\n", sum);
+    PrintFmt("Part 1: {}\n", sum);
+
+    struct S
+    {
+      std::string label;
+      s64 value;
+    };
+    FixedArray<UnboundedArray<S>, 256> boxes;
+
+    for (auto &entry : splits)
+    {
+      bool set = entry.contains('=');
+      auto vars = Split(entry, "=-", KeepEmpty::No);
+      auto label = vars[0];
+
+      auto hash = Hash(label);
+      auto &box = boxes[hash];
+
+      if (set)
+      {
+        auto value = StrToNum(vars[1]);
+        if (auto i = box.FindFirst([&label](auto &&e) { return e.label == label; }); i >= 0)
+          { box[i].value = value; }
+        else
+          { box.Append({ label, value }); }
+      }
+      else
+      {
+        if (auto i = box.FindFirst([&label](auto &&e) { return e.label == label; }); i >= 0)
+          { box.RemoveAt(i); }
+      }
+    }
+
+    s64 total = 0;
+    for (s32 i = 0; i < boxes.Count(); i++)
+    {
+      auto &box = boxes[i];
+
+      if (box.IsEmpty())
+        { continue; }
+
+      for (auto b = 0; b < box.Count(); b++)
+      {
+        auto &e = box[b];
+        auto power = (i + 1) * (b + 1) * e.value;
+        total += power;
+      }
+    }
+
+    PrintFmt("Part 2: {}\n", total);
   }
 }

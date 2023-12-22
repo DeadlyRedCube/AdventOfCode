72dfd85e1975e27fd8dd021608eca61f9fc80895
DeadlyRedCube <josh@drilsej.com>
2023-12-21 20:54:23 -0800

    2023 Day 21 Part 2 Redux
    Okay this solution takes advantage of properties of the real data (which are not present in the sample data), which I don't love, but whatever it's a LOT faster

--- 2023/AOC2023/D21.h
+++ 2023/AOC2023/D21.h
@@ -5,6 +5,8 @@ namespace D21
   void Run(const char *path)
   {
     auto grid = ReadFileAsCharArray(path);
+    Array2D<s64> distances = { grid.Width(), grid.Height() };
+    distances.Fill(std::numeric_limits<s64>::max());
 
     Vec2S64 sPosition;
     bool foundS = false;
@@ -24,103 +26,32 @@ namespace D21
         { break; }
     }
 
-    // Now trace the distances through the graph
-    enum class SpaceType
+    s64 p1Count = 0;
+
+
+    const s64 p2TargetStepCount = 26501365;
+
+    // For part 2: some counts that I'll explain later
+    s64 p2CenterOnParityCount = 0;
+    s64 p2CenterOffParityCount = 0;
+    s64 p2OtherDiamondBothParityCount = 0;
     {
-      Unreached,
-      Even,
-      Odd,
-    };
+      Vec2S64 dirs[] = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
 
-    struct Step
-    {
-      Vec2S64 pos;
-      s64 step = 0;
-
-      bool operator < (const Step &b) const
-        { return step > b.step; }
-    };
-
-    Vec2S64 dirs[] = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
-
-    struct Found
-    {
-      Vec2S64 pos;
-      SpaceType type;
-
-      bool operator==(const Found &) const = default;
-      auto operator<=>(const Found &) const = default;
-    };
-
-
-    {
-      std::queue<Step> stepQueue;
-      stepQueue.push({ .pos = sPosition, .step = 0 });
-
-      struct Found
+      struct Step
       {
         Vec2S64 pos;
-        SpaceType type;
+        s64 step = 0;
 
-        bool operator==(const Found &) const = default;
-        auto operator<=>(const Found &) const = default;
+        bool operator < (const Step &b) const
+          { return step > b.step; }
       };
 
-      std::set<Found> foundSteps;
+
+      std::priority_queue<Step> stepQueue;
+      stepQueue.push({ .pos = sPosition, .step = 0 });
 
       while (!stepQueue.empty())
-      {
-        auto step = stepQueue.front();
-        stepQueue.pop();
-
-        auto type = (step.step & 1) ? SpaceType::Odd : SpaceType::Even;
-
-        if (!foundSteps.insert({ .pos = step.pos, .type = type }).second)
-          { continue; }
-
-        if (step.step >= 64)
-          { continue; }
-
-        for (auto dir : dirs)
-        {
-          if (grid.PositionInRange(step.pos + dir) && grid[step.pos + dir] != '#')
-            { stepQueue.push({ step.pos + dir, step.step + 1 }); }
-        }
-      }
-
-      // Count the evens
-      s64 count = 0;
-      for (auto &&f : foundSteps)
-        { if (f.type == SpaceType::Even) { count++; } }
-
-      PrintFmt("Part 1: {}\n", count);
-    }
-
-    const s64 targetStepCount = 26501365;
-
-    // Alright this all sucks but it got the right answer.
-    // I discovered that out past a certain point (a 3x3 grid in the real data, but I needed more in the sample),
-    //  the distance to any given grid square increases by the manhattan distance to that tile (likely due to the
-    //  straightline channels around each grid square).
-    s32 gridSizeMultiplier = 3;
-
-    // For the sample I needed a larger grid to get the edges right
-    while (gridSizeMultiplier * grid.Width() < 77)
-      { gridSizeMultiplier += 2; }
-
-    ssz halfGridSizeMultiplier = gridSizeMultiplier / 2;
-    Vec2S64 gridCenterOffset = { grid.Width() * halfGridSizeMultiplier, grid.Height() * halfGridSizeMultiplier };
-
-    Array2D<s64> distances = { grid.Width() * gridSizeMultiplier, grid.Height() * gridSizeMultiplier };
-    distances.Fill(std::numeric_limits<s64>::max());
-
-    // Generate the distances for our embiggened grid
-    {
-      auto sBigPosition = sPosition + gridCenterOffset;
-      std::priority_queue<Step> stepQueue;
-      stepQueue.push({ sBigPosition, 0 });
-
-      while(!stepQueue.empty())
       {
         auto step = stepQueue.top();
         stepQueue.pop();
@@ -130,92 +61,136 @@ namespace D21
 
         distances[step.pos] = step.step;
 
+        if (step.step <= 64)
+        {
+          // Count up all the step lengtsh <= 64 for part 1
+          if ((step.step & 1) == 0)
+            { p1Count++; }
+        }
+
+        // Now tally up the part 2 counts (more on what's going on here after this loop)
+        if (step.step <= grid.Width() / 2)
+        {
+          // For part 2, there's a diamond pattern, count everything inside the diamond (manhattan distance of
+          //  width / 2) at each even/odd parity
+          if ((step.step & 1) == (p2TargetStepCount & 1))
+            { p2CenterOnParityCount++; }
+          else
+            { p2CenterOffParityCount++; }
+        }
+        else
+        {
+          // Outside of the diamond, we care about the corners, but because every even/odd parity corner piece comes in
+          //  pairs, we'll just tally up literally all the walkable space in those outer portions.
+          p2OtherDiamondBothParityCount++;
+        }
+
         for (auto dir : dirs)
         {
-          if (distances.PositionInRange(step.pos + dir) &&  grid[grid.Wrap(step.pos + dir)] != '#')
+          // Queue up any neighboring directions that are on-grid.
+          if (grid.PositionInRange(step.pos + dir) && grid[step.pos + dir] != '#')
             { stepQueue.push({ step.pos + dir, step.step + 1 }); }
         }
       }
+
+      PrintFmt("Part 1: {}\n", p1Count);
     }
 
-    // Now we want to get a list of the (reachable!) evens/odds within a square (so we can add entire tile counts
-    //  wholesale through the middle where we know all the distances are in range)
-    s64 centerOnParityCount = 0;
-    s64 centerOffParityCount = 0;
-    for (ssz y = 0; y < grid.Height(); y++)
     {
-      for (ssz x = 0; x < grid.Width(); x++)
-      {
-        auto dist = distances[{x + halfGridSizeMultiplier * grid.Width(), y + halfGridSizeMultiplier * grid.Height() }];
-        if (dist == std::numeric_limits<s64>::max())
-          { continue; }
+      // In the actual puzzle input (although not the example, blah), the input has diamond channels cut in it:
+      //
+      //  AAAA.E.BBBB
+      //  AAA.EEE.BBB
+      //  AA.EEEEE.BB
+      //  A.EEEEEEE.B
+      //  .EEEEEEEEE.
+      //  C.EEEEEEE.D
+      //  CC.EEEEE.DD
+      //  CCC.EEE.DDD
+      //  CCCC.E.DDDD
+      //
+      // We're going to count up how many reachable odd-step squares there are in the E segment
+      //  (p2CenterOnParityCount), how many reachable even-step squares there are "p2CenterOffParityCount", and then
+      //  how many of both odds and evens we can touch in the remaining regions. More on that later.
 
-        if ((dist & 1) == (targetStepCount & 1))
-          { centerOnParityCount++; }
-        else
-          { centerOffParityCount++; }
-      }
+      ASSERT(grid.Width() == grid.Height());                      // Square!
+      ASSERT(targetStepCount % grid.Width() == grid.Width() / 2); // For this fast solution to work, this must be true.
+
+      // Every grid.Width steps we add an additional ring of the diamonds we've measured to the list.
+      const s64 stepExtent = p2TargetStepCount / grid.Width();
+
+      // Since the initial step size is half the grid width (65 in the actual input), we count up all of the odd
+      //  reachable squares in the E region of the above diagram at the center.
+      // If we add another grid width worth of step (65 + 131 = 196) we'd be looking at these regions
+      //  (extra spaces added between grid squares to make them easier to make out, "-" means "not counted", and
+      //  lowercase are off-parity (i.e. count the evens instead of the odds):
+      // ----.-.---- ----.e.---- ----.-.----
+      // ---.---.--- ---.eee.--- ---.---.---
+      // --.-----.-- --.eeeee.-- --.-----.--
+      // -.-------.- -.eeeeeee.- -.-------.-
+      // .---------. .eeeeeeeee. .---------.
+      // ----------- eeeeeeeeeee -----------
+      // .---------. .eeeeeeeee. .---------.
+      // -.-------.D c.eeeeeee.d C.-------.-
+      // --.-----.DD cc.eeeee.dd CC.-----.--
+      // ---.---.DDD ccc.eee.ddd CCC.---.---
+      // ----.-.DDDD cccc.e.dddd CCCC.-.----
+
+      // ----.e.bbbb AAAA.E.BBBB aaaa.e.----
+      // ---.eee.bbb AAA.EEE.BBB aaa.eee.---
+      // --.eeeee.bb AA.EEEEE.BB aa.eeeee.--
+      // -.eeeeeee.b A.EEEEEEE.B a.eeeeeee.-
+      // .eeeeeeeee. .EEEEEEEEE. .eeeeeeeee.
+      // eeeeeeeeeee EEEEEEEEEEE eeeeeeeeeee
+      // .eeeeeeeee. .EEEEEEEEE. .eeeeeeeee.
+      // -.eeeeeee.D C.EEEEEEE.D C.eeeeeee.-
+      // --.eeeee.DD CC.EEEEE.DD CC.eeeee.--
+      // ---.eee.DDD CCC.EEE.DDD CCC.eee.---
+      // ----.e.DDDD CCCC.E.DDDD CCCC.e.----
+
+      // ----.-.BBBB aaaa.e.bbbb AAAA.-.----
+      // ---.---.BBB aaa.eee.bbb AAA.---.---
+      // --.-----.BB aa.eeeee.bb AA.-----.--
+      // -.-------.B a.eeeeeee.b A.-------.-
+      // .---------. .eeeeeeeee. .---------.
+      // ----------- eeeeeeeeeee -----------
+      // .---------. .eeeeeeeee. .---------.
+      // -.-------.- -.eeeeeee.- -.-------.-
+      // --.-----.-- --.eeeee.-- --.-----.--
+      // ---.---.--- ---.eee.--- ---.---.---
+      // ----.-.---- ----.e.---- ----.-.----
+
+      // in that ring we end up adding:
+      //  4x "e" regions (count the evens)
+      //  2x "A", "a", "B", "b", "C", "c", "D", and "d" regions (equal parts of each, of both parities)
+      //
+      // If we add another ring (not going to draw that diagram), we end up adding:
+      //  8x "E" regions (count the odds again)
+      //  4x "A", "a", "B", "b", "C", "c", "D", and "d" regions (again equal parts of each, both parities)
+      //
+      // Next ring:
+      // 12x "e" regions (back to the evens)
+      //  6x "A", "a", "B", "b", "C", "c", "D", and "d" regions (same as before, equal of all)
+      //
+      // So every Nth multiple of gridWidth we add, we add N*4 "E" or "e" regions (alternating) and N*2 of the other
+      //  regions (which is the same as saying we add N*2 of an "AbCd" diamond and N*2 of an "aBcD" diamond.
+      //
+      // The closed form of each of those (taking advantage of integer division for the center counts)
+
+      // ECount (count of "E" regions): = 1 1 9 9 25 25 49 49 ...
+      s64 ECount = Square(2*(stepExtent / 2) + 1);
+
+      // eCount (count of "e" regions): = 0 4 4 16 16 36 36 64 64 ...
+      s64 eCount = Square(2*((stepExtent + 1) / 2));
+
+      // aAbBcCdDCount (count of all other regions) = 0 2 6 12 20 30 42 ...
+      s64 aAbBcCdDCount = (ECount + eCount) / 2;
+
+      // Summing allllll that up (multiplied by the values in each square) gives us:
+      s64 p2Count = ECount * p2CenterOnParityCount
+        + eCount * p2CenterOffParityCount
+        + aAbBcCdDCount * p2OtherDiamondBothParityCount;
+      PrintFmt("Part 2: {}\n", p2Count);
     }
-
-    s64 count = 0;
-
-    // Below a tile-index manhattan distance of this distance we know that the entire grid is in range and we can
-    //  skip doing an actual check against it.
-    ssz minTileExtent = targetStepCount / grid.Width() - 1;
-
-    // Above this distance, the entire tile is guaranteed to be out of range.
-    ssz maxTileExtent = (targetStepCount + grid.Width() - 1) / grid.Width();
-
-    for (ssz ty = -maxTileExtent; ty <= maxTileExtent; ty++)
-    {
-      // Figure out the x extents to that we need to test (Basically testing a diamond around the whole thing)
-      ssz xMinExtent = std::max(ssz(0), minTileExtent - std::abs(ty));
-      ssz xMaxExtent = maxTileExtent - std::abs(ty);
-
-      if (xMinExtent > 0)
-      {
-        // Inside of this extent it's full squares only!
-        count += (xMinExtent - 1) * (centerOffParityCount + centerOnParityCount);
-        count += centerOnParityCount;
-      }
-
-      // Time to tally up the ones we actually need to examine closely
-      for (ssz absTx = xMinExtent; absTx <= xMaxExtent; absTx++)
-      {
-        // Test both positive and negative versions of our x (only do one if we're at 0)
-        ssz xArray[] = { -absTx, absTx };
-        ArrayView<ssz> xList = (absTx == 0) ? ArrayView<ssz>(xArray, 1) : ArrayView<ssz>(xArray, 2);
-
-        for (ssz tx : xList)
-        {
-          // Get the x, y coordinate of the tile we're going to actually investigate
-          ssz tileX = std::clamp(tx + halfGridSizeMultiplier, ssz(0), ssz(gridSizeMultiplier - 1));
-          ssz tileY = std::clamp(ty + halfGridSizeMultiplier, ssz(0), ssz(gridSizeMultiplier - 1));
-
-          // The full manhattan distance to this tile from the tile we're testing against  (the baseline distance
-          //  we're going to use)
-          ssz tileDistance = std::abs(tx + halfGridSizeMultiplier - tileX) * grid.Width()
-            + std::abs(ty + halfGridSizeMultiplier - tileY) * grid.Height();
-
-          for (ssz y = 0; y < grid.Height(); y++)
-          {
-            for (ssz x = 0; x < grid.Width(); x++)
-            {
-              auto dist = distances[{x + tileX * grid.Width(), y + tileY * grid.Height() }];
-
-              // ignore unreachable spaces
-              if (dist == std::numeric_limits<s64>::max())
-                { continue; }
-
-              dist += tileDistance;
-              if (dist <= targetStepCount && (dist & 1) == (targetStepCount & 1))
-                { count++; }
-            }
-          }
-        }
-      }
-    }
-
-    PrintFmt("Part 2:{}\n", count);
   }
 }

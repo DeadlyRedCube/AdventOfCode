1f9d0a3e3b7e7821592244ee51bce5c18cf899ff
DeadlyRedCube <josh@drilsej.com>
2023-12-24 04:13:05 -0800

    2023 Day 24 Part 2
    I did it, my math was actually right but I had a bad set of velocity values causing a NaN. Ugh.

--- 2023/AOC2023/D24.h
+++ 2023/AOC2023/D24.h
@@ -65,118 +65,80 @@ namespace D24
 
     // need to find a single position that will collide with every hailstone
 
+    // We want 3 indices that have different x, y, and z velocity values for ... reasons that I'm too tired to figure
+    //  out but doing this makes it not become a NaN at the end.
+    // (Honestly I'm not sure that the problem isn't actually something else - should probably just be doing a
+    //  denominator check on the divisions and moving on to different values if we get a (near) 0), but meh. It worked
+    //  and I'm moving on.
+    s32 s0Index = 0;
+    s32 s1Index = 1;
+    while (stones[s0Index].vel == stones[s1Index].vel )
+      { s1Index++; }
+
+    s32 s2Index = s1Index + 1;
+    while (stones[s0Index].vel == stones[s2Index].vel || stones[s1Index].vel == stones[s2Index].vel)
+      { s2Index++; }
+
     // for any given stone, A = A0 + Av*t, our ray (P + Qt) should have a matching t value.
     // With three stones, we have nine equations and nine unknowns (t, u, v, Px, Py, Pz, Qx, Qy, Qz), assuming that
     //  any solution for three will work for all:
     // A0x + Avx*t = Px + Qx*t
     // A0y + Avy*t = Py + Qy*t
     // A0z + Avz*t = Pz + Qz*t
-    double A0x = double(stones[0].pos.x);
-    double A0y = double(stones[0].pos.y);
-    double A0z = double(stones[0].pos.z);
-    double Avx = double(stones[0].vel.x);
-    double Avy = double(stones[0].vel.y);
-    double Avz = double(stones[0].vel.z);
+    double A0x = double(stones[s0Index].pos.x);
+    double A0y = double(stones[s0Index].pos.y);
+    double A0z = double(stones[s0Index].pos.z);
+    double Avx = double(stones[s0Index].vel.x);
+    double Avy = double(stones[s0Index].vel.y);
+    double Avz = double(stones[s0Index].vel.z);
     //
     // B0x + Bvx*u = Px + Qx*u
     // B0y + Bvy*u = Py + Qy*u
     // B0z + Bvz*u = Pz + Qz*u
-    double B0x = double(stones[1].pos.x);
-    double B0y = double(stones[1].pos.y);
-    double B0z = double(stones[1].pos.z);
-    double Bvx = double(stones[1].vel.x);
-    double Bvy = double(stones[1].vel.y);
-    double Bvz = double(stones[1].vel.z);
+    double B0x = double(stones[s1Index].pos.x);
+    double B0y = double(stones[s1Index].pos.y);
+    double B0z = double(stones[s1Index].pos.z);
+    double Bvx = double(stones[s1Index].vel.x);
+    double Bvy = double(stones[s1Index].vel.y);
+    double Bvz = double(stones[s1Index].vel.z);
     //
     // C0x + Cvx*v = Px + Qx*v
     // C0y + Cvy*v = Py + Qy*v
     // C0z + Cvz*v = Pz + Qz*v
-    double C0x = double(stones[3].pos.x);
-    double C0y = double(stones[3].pos.y);
-    double C0z = double(stones[3].pos.z);
-    double Cvx = double(stones[3].vel.x);
-    double Cvy = double(stones[3].vel.y);
-    double Cvz = double(stones[3].vel.z);
+    double C0x = double(stones[s2Index].pos.x);
+    double C0y = double(stones[s2Index].pos.y);
+    double C0z = double(stones[s2Index].pos.z);
+    double Cvx = double(stones[s2Index].vel.x);
+    double Cvy = double(stones[s2Index].vel.y);
+    double Cvz = double(stones[s2Index].vel.z);
     //
     // We can eliminiate t, u, and v and end up with 6 equations with 6 unknowns (Px, Py, Pz, Qx, Qy, Qz):
     // (Px - A0x) / (Avx - Qx) = (Py - A0y) / (Avy - Qy) = (Pz - A0z) / (Avz - Qz)
     // (Px - B0x) / (Bvx - Qx) = (Py - B0y) / (Bvy - Qy) = (Pz - B0z) / (Bvz - Qz)
     // (Px - C0x) / (Cvx - Qx) = (Py - C0y) / (Cvy - Qy) = (Pz - C0z) / (Cvz - Qz)
 
-    // (Px - A0x) * (Avy - Qy) = (Py - A0y) * (Avx - Qx)
-    // (Px - A0x) * (Avz - Qz) = (Pz - A0z) * (Avx - Qx)
-    //
-    // (Px - B0x) * (Bvy - Qy) = (Py - B0y) * (Bvx - Qx)
-    // (Px - B0x) * (Bvz - Qz) = (Pz - B0z) * (Bvx - Qx)
-    //
-    // (Px - C0x) * (Cvy - Qy) = (Py - C0y) * (Cvx - Qx)
-    // (Px - C0x) * (Cvz - Qz) = (Pz - C0z) * (Cvx - Qx)
-    //
-    // Px = [(Py - A0y) * (Avx - Qx)] / [Avy - Qy] + A0x   =   [(Pz - A0z) * (Avx - Qx)] / [Avz - Qz] + A0x
-    //
-    // [Avz - Qz][Py - A0y] = [Avy - Qy][Pz - A0z]
-    // [Bvz - Qz][Py - B0y] = [Bvy - Qy][Pz - B0z]
-    // [Cvz - Qz][Py - C0y] = [Cvy - Qy][Pz - C0z]
-    // Try to whittle down to Py here:
-    //
-    // Py = [Avy - Qy][Pz - A0z]/[Avz - Qz] + A0y
-    //    = [Bvy - Qy][Pz - B0z]/[Bvz - Qz] + B0y
-    //    = [Cvy - Qy][Pz - C0z]/[Cvz - Qz] + C0y
-    //
-    // [Avy - Qy][Pz - A0z]/[Avz - Qz] + A0y = [Bvy - Qy][Pz - B0z]/[Bvz - Qz] + B0y
-    // [Avy - Qy][Pz - A0z]/[Avz - Qz] + A0y = [Cvy - Qy][Pz - C0z]/[Cvz - Qz] + C0y
-    //
-    // Pz = ([Bvy - Qy][Pz - B0z]/[Bvz - Qz] + B0y - A0y)(Avz - Qz)/(Avy - Qy) + A0z
-    //    = ([Cvy - Qy][Pz - C0z]/[Cvz - Qz] + C0y - C0y)(Avz - Qz)/(Avy - Qy) + A0z
-    //
-    // i hate this
-    //
-    // Well technically I have solved for Px, Py, and Pz so let's put this frankenstein nightmare back together and see
-    //
-    // okay fuck it, we're gonna leverage more hailstones! So you get NOTHING, sample problem!
-    //
-    // [Avz - Qz][Py - A0y] = [Avy - Qy][Pz - A0z] =>
-    // Py = [Avy - Qy][Pz - A0z]/[Avz - Qz] + A0y
-    //    = [Bvy - Qy][Pz - B0z]/[Bvz - Qz] + B0y
-    //
-    // [Avy - Qy][Pz - A0z]/[Avz - Qz] + A0y = [Bvy - Qy][Pz - B0z]/[Bvz - Qz] + B0y
-    // [Bvz - Qz][Avy - Qy][Pz - A0z] + [Bvz - Qz][Avz - Qz]A0y = [Avz - Qz][Bvy - Qy][Pz - B0z] + [Bvz - Qz][Avz - Qz]B0y
-    // [Bvz - Qz][Avy - Qy][Pz - A0z] = [Avz - Qz][Bvy - Qy][Pz - B0z] + [Bvz - Qz][Avz - Qz](B0y - A0y)
-    // [Bvz - Qz][Avy - Qy]Pz - [Bvz - Qz][Avy - Qy]A0z = [Avz - Qz][Bvy - Qy]Pz - [Avz - Qz][Bvy - Qy]B0z + [Bvz - Qz][Avz - Qz][B0y - A0y]
-    // ([Bvz - Qz][Avy - Qy] - [Avz - Qz][Bvy - Qy])Pz = [Bvz - Qz][Avy - Qy]A0z - [Avz - Qz][Bvy - Qy]B0z + [Bvz - Qz][Avz - Qz][B0y - A0y]
-    // Pz = ([Bvz - Qz][Avy - Qy]A0z - [Avz - Qz][Bvy - Qy]B0z + [Bvz - Qz][Avz - Qz][B0y - A0y]) / ([Bvz - Qz][Avy - Qy] - [Avz - Qz][Bvy - Qy])
-    //    = ([Cvz - Qz][Avy - Qy]A0z - [Avz - Qz][Cvy - Qy]C0z + [Cvz - Qz][Avz - Qz][C0y - A0y]) / ([Cvz - Qz][Avy - Qy] - [Avz - Qz][Cvy - Qy])
-    //
-    // ffffff three to go and I think shit's about to go quadratic
-    //
-    /// ... gonna try a different way.
-    //
-    // BACK TO THIS ugh
-    //
-    // (Px - A0x) / (Avx - Qx) = (Py - A0y) / (Avy - Qy) = (Pz - A0z) / (Avz - Qz)
-    // (Px - B0x) / (Bvx - Qx) = (Py - B0y) / (Bvy - Qy) = (Pz - B0z) / (Bvz - Qz)
-    // (Px - C0x) / (Cvx - Qx) = (Py - C0y) / (Cvy - Qy) = (Pz - C0z) / (Cvz - Qz)
-
-    // Rearranging:
+    // Rearranging the Px/Py pairing:
 
     // Px * Avy - Px * Qy - A0x * Avy + A0x * Qy = Py * Avx - Py * Qx - A0y * Avx + A0y * Qx
     // (Px * Qy - Py * Qx) = (Px * Avy - Py * Avx) + (A0y * Avx - A0x * Avy) + (A0x * Qy - A0y * Qx)
     // (Px * Qy - Py * Qx) = (Px * Bvy - Py * Bvx) + (B0y * Bvx - B0x * Bvy) + (B0x * Qy - B0y * Qx)
     // (Px * Qy - Py * Qx) = (Px * Cvy - Py * Cvx) + (C0y * Cvx - C0x * Cvy) + (C0x * Qy - C0y * Qx)
     //
-    // oh shit I think this might work, Do the same thing for Px/Pz:
+    // Note that this gets a common (Px * Qy - Py * Qx) on the left side of everything, and the right side of each is
+    //  now just a linear equation.
+    // Do the same for the Pz/Px and Py/Pz pairints:
     //
     // (Pz * Qx - Px * Qz) = (Pz * Avx - Px * Avz) + (A0x * Avz - A0z * Avx) + (A0z * Qx - A0x * Qz)
     // (Pz * Qx - Px * Qz) = (Pz * Bvx - Px * Bvz) + (B0x * Bvz - B0z * Bvx) + (B0z * Qx - B0x * Qz)
     // (Pz * Qx - Px * Qz) = (Pz * Cvx - Px * Cvz) + (C0x * Cvz - C0z * Cvx) + (C0z * Qx - C0x * Qz)
-
-    // and then Py/Pz:
     //
     // (Py * Qz - Pz * Qy) = (Py * Avz - Pz * Avy) + (A0z * Avy - A0y * Avz) + (A0y * Qz - A0z * Qy)
     // (Py * Qz - Pz * Qy) = (Py * Bvz - Pz * Bvy) + (B0z * Bvy - B0y * Bvz) + (B0y * Qz - B0z * Qy)
     // (Py * Qz - Pz * Qy) = (Py * Cvz - Pz * Cvy) + (C0z * Cvy - C0y * Cvz) + (C0y * Qz - C0z * Qy)
     //
-    // YESSSS okay now I think I can turn this into 6 boring-ass linear equations:
+    // This now turns into a series of 6 straight-up linear equations, which we can solve in, you know, the normal way.
+    //  (don't worry I'm still gonna make it look difficult because it's late and I forget all the better math trickery
+    //  to do this)
     // [Avy - Bvy]Px - [Avx - Bvx]Py - [A0y - B0y]Qx + [A0x - B0x]Qy = (B0y * Bvx - B0x * Bvy) - (A0y * Avx - A0x * Avy)
     // [Avy - Cvy]Px - [Avx - Cvx]Py - [A0y - C0y]Qx + [A0x - C0x]Qy = (C0y * Cvx - C0x * Cvy) - (A0y * Avx - A0x * Avy)
     // [Avx - Bvx]Pz - [Avz - Bvz]Px - [A0x - B0x]Qz + [A0z - B0z]Qx = (B0x * Bvz - B0z * Bvx) - (A0x * Avz - A0z * Avx)
@@ -185,7 +147,6 @@ namespace D24
     // [Avz - Cvz]Py - [Avy - Cvy]Pz - [A0z - C0z]Qy + [A0y - C0y]Qz = (C0z * Cvy - C0y * Cvz) - (A0z * Avy - A0y * Avz)
     //
     // Combine some terms to get:
-
     double abvx = Avx - Bvx;
     double abvy = Avy - Bvy;
     double abvz = Avz - Bvz;
@@ -208,6 +169,7 @@ namespace D24
     double h3 = (C0x * Cvz - C0z * Cvx) - (A0x * Avz - A0z * Avx);
     double h4 = (B0z * Bvy - B0y * Bvz) - (A0z * Avy - A0y * Avz);
     double h5 = (C0z * Cvy - C0y * Cvz) - (A0z * Avy - A0y * Avz);
+
     // abvy*Px - abvx*Py - ab0y*Qx + ab0x*Qy = h0
     // acvy*Px - acvx*Py - ac0y*Qx + ac0x*Qy = h1
     // abvx*Pz - abvz*Px - ab0x*Qz + ab0z*Qx = h2
@@ -215,9 +177,7 @@ namespace D24
     // abvz*Py - abvy*Pz - ab0z*Qy + ab0y*Qz = h4
     // acvz*Py - acvy*Pz - ac0z*Qy + ac0y*Qz = h5
 
-
-    // HAHAHAHAHAHA I DID IT (so far)
-    //
+    // Now we're going to take each pair of those eliminate its initial P variable (leaving just the other)
     // Okay now that's 6 linear equations and 6 variable, right?
     //
     // Px = [h0 + abvx*Py + ab0y*Qx - ab0x*Qy]/abvy
@@ -247,12 +207,13 @@ namespace D24
     // Pz = ([abvz*ac0z - acvz*ab0z]*Qy + [acvz*ab0y - abvz*ac0y)*Qz + [abvz*h5 - acvz*h4])/(acvz*abvy - abvz*acvy)
     // -----------
     //
-    // Alright we can sub this shit in for Px, Py, and Pz to get everything in terms of Qx, Qy, and Qz
+    // Alright, now we can sub these into (half of) our original linear equations and rearrange in terms of Qx, Qy,
+    //  and Qz, leaving us with three equations and three variables.
     // abvy*Px - abvx*Py - ab0y*Qx + ab0x*Qy = h0
     // abvx*Pz - abvz*Px - ab0x*Qz + ab0z*Qx = h2
     // abvz*Py - abvy*Pz - ab0z*Qy + ab0y*Qz = h4
     //
-    // Make some more variables!
+    // Make some more variables to make this easier
     // Px = (Pxz*Qz + Pxx*Qx + Pxc)/Pxd
     // Py = (Pyx*Qx + Pyy*Qy + Pyc)/Pyd
     // Pz = (Pzy*Qy + Pzz*Qz + Pzc)/Pzd
@@ -276,11 +237,14 @@ namespace D24
     // abvx*[(Pzy*Qy + Pzz*Qz + Pzc)/Pzd] - abvz*[(Pxz*Qz + Pxx*Qx + Pxc)/Pxd] - ab0x*Qz + ab0z*Qx = h2
     // abvz*[(Pyx*Qx + Pyy*Qy + Pyc)/Pyd] - abvy*[(Pzy*Qy + Pzz*Qz + Pzc)/Pzd] - ab0z*Qy + ab0y*Qz = h4
     //
-    // okay this is just garbage math now but it's finally feeling doable
+    // okay this is unintelligible garbage now but we're almost there:
     //
-    // ([abvy/Pxd]*Pxz)*Qz + ([abvy/Pxd]*Pxx - [abvx/Pyd]*Pyx - ab0y)*Qx + (ab0x - [abvx/Pyd]*Pyy)*Qy = h0 - [abvy/Pxd]*Pxc + [abvx/Pyd]*Pyc
-    // ([abvx/Pzd]*Pzy)*Qy + ([abvx/Pzd]*Pzz - [abvz/Pxd]*Pxz - ab0x)*Qz + (ab0z - [abvz/Pxd]*Pxx)*Qx = h2 - [abvx/Pzd]*Pzc + [abvz/Pxd]*Pxc
-    // ([abvz/Pyd]*Pyx)*Qx + ([abvz/Pyd]*Pyy - [abvy/Pzd]*Pzy - ab0z)*Qy + (ab0y - [abvy/Pzd]*Pzz)*Qz = h4 - [abvz/Pyd]*Pyc + [abvy/Pzd]*Pzc
+    // ([abvy/Pxd]*Pxz)*Qz + ([abvy/Pxd]*Pxx - [abvx/Pyd]*Pyx - ab0y)*Qx + (ab0x - [abvx/Pyd]*Pyy)*Qy
+    //   = h0 - [abvy/Pxd]*Pxc + [abvx/Pyd]*Pyc
+    // ([abvx/Pzd]*Pzy)*Qy + ([abvx/Pzd]*Pzz - [abvz/Pxd]*Pxz - ab0x)*Qz + (ab0z - [abvz/Pxd]*Pxx)*Qx
+    //   = h2 - [abvx/Pzd]*Pzc + [abvz/Pxd]*Pxc
+    // ([abvz/Pyd]*Pyx)*Qx + ([abvz/Pyd]*Pyy - [abvy/Pzd]*Pzy - ab0z)*Qy + (ab0y - [abvy/Pzd]*Pzz)*Qz
+    //   = h4 - [abvz/Pyd]*Pyc + [abvy/Pzd]*Pzc
     //
     // MOAR VARIABLES
     auto Qz0 = (abvy/Pxd)*Pxz;
@@ -313,12 +277,21 @@ namespace D24
     // ([Qx2*Qy0 - Qx0*Qy2][Qx0*Qz1 - Qx1*Qz0] - [Qx1*Qy0 - Qx0*Qy1][Qx0*Qz2 - Qx2*Qz0])Qz
     //   = [Qx1*Qy0 - Qx0*Qy1][Qx2*r0 - Qx0*r2] - [Qx2*Qy0 - Qx0*Qy2][Qx1*r0 - Qx0*r1]
 
-    // THIS DOES NOT WORK AND I AM LOSING MY GODDAMN MIND
-    //
-    // where did i mess up
-    //
-    // WHY IS THE DENOMENATOR ZERO WHAT DID I DO TO DESERVE THIS, IT'S CHRISTMAS
+    // Alright after alllll that we can now solve for Qz, and then backsolve for everything else up the chain.
     double Qz = ((Qx1*Qy0 - Qx0*Qy1)*(Qx2*r0 - Qx0*r2) - (Qx2*Qy0 - Qx0*Qy2)*(Qx1*r0 - Qx0*r1))
       / ((Qx2*Qy0 - Qx0*Qy2)*(Qx0*Qz1 - Qx1*Qz0) - (Qx1*Qy0 - Qx0*Qy1)*(Qx0*Qz2 - Qx2*Qz0));
+
+    double Qy = ((Qx0*Qz1 - Qx1*Qz0)*Qz + (Qx1*r0 - Qx0*r1))/(Qx1*Qy0 - Qx0*Qy1);
+
+    double Qx = (r0 - Qy0*Qy - Qz0*Qz)/Qx0;
+
+    double Px = (Pxz*Qz + Pxx*Qx + Pxc)/Pxd;
+    double Py = (Pyx*Qx + Pyy*Qy + Pyc)/Pyd;
+    double Pz = (Pzy*Qy + Pzz*Qz + Pzc)/Pzd;
+
+    // Now, sum the (rounded, to deal with float precision issues) coordinates of the starting position together.
+    //  The end. FINALLY.
+    s64 sum = s64(std::round(Px)) + s64(std::round(Py)) + s64(std::round(Pz));
+    PrintFmt("Part 2: {}\n", sum);
   }
 }

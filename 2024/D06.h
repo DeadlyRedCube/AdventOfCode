d17955f16a0a5a4c0019d1ec9ccb774824d293bf
Josh Jersild <josh@drilsej.com>
2024-12-05 22:55:17 -0800

    2024 Day 6 solution 3 (runs in 29ms)
    Rather than using sets, use a grid (and some flags for direction visiting) and go back to the original simpler logic of just iterating through the grid and now it's even faster (29ms)

--- 2024/D06.h
+++ 2024/D06.h
@@ -3,130 +3,144 @@
 
 namespace D06
 {
+  enum class Dir
+  {
+    N,
+    E,
+    S,
+    W,
+  };
+
+  enum class DirFlag
+  {
+    N = 0x1,
+    E = 0x8,
+    S = 0x2,
+    W = 0x4,
+
+    None = 0,
+    All = 0xf,
+  };
+
   void Run(const char *path)
   {
+    s64 p1 = 0;
+    s64 p2 = 0;
+
     auto grid = ReadFileAsCharArray(path);
 
-    std::set<Vec2<s32>> visitedPositions;
     Vec2<s32> initialGuardPos;
 
-    std::vector<std::vector<s32>> blockYsPerX;
-    std::vector<std::vector<s32>> blockXsPerY;
-
-    blockYsPerX.resize(grid.Width());
-    blockXsPerY.resize(grid.Height());
-
+    // Start by finding the initial guard pos.
     for (s32 y = 0; y < grid.Height(); y++)
     {
+      bool found = false;
       for (s32 x = 0; x < grid.Width(); x++)
       {
         if (grid[x, y] == '^')
         {
           initialGuardPos = { x, y };
+          found = true;
           break;
         }
-
-        if (grid[x, y] == '#')
-        {
-          blockYsPerX[x].push_back(y);
-          blockXsPerY[y].push_back(x);
-        }
-      }
-    }
-
-    visitedPositions.insert(initialGuardPos);
-    auto guardPos = initialGuardPos;
-    Vec2<s32> dir = { 0, -1 };
-
-    // Use either a row or column of (sorted) block coordinates to find the next block along the direction
-    auto FindNextBlock = [](auto &cs, auto c, auto d, auto max)
-    {
-      auto found = std::ranges::lower_bound(cs, c);
-      if (d < 0)
-      {
-        if (found == cs.begin())
-          { return -1; } // No blocks before our element
-        else
-          { return *(--found); } // the coordinate before the one we found is correct.
-      }
-      else
-      {
-        if (found == cs.end())
-          { return s32(max); } // No blocks after our element
-        else
-          { return *found; } // The one we found is next (we know it won't find the input square because it must be free)
-      }
-    };
-
-    while (true)
-    {
-      // Find the next block along this direction
-      auto nextBlock = (dir.x != 0)
-        ? Vec2{ FindNextBlock(blockXsPerY[guardPos.y], guardPos.x, dir.x, grid.Width()), guardPos.y }
-        : Vec2{ guardPos.x, FindNextBlock(blockYsPerX[guardPos.x], guardPos.y, dir.y, grid.Height()) };
-
-      // Add all of the positions along the way into the set until our next step is either off the edge or to the block
-      while (guardPos + dir != nextBlock)
-      {
-        guardPos += dir;
-        visitedPositions.insert(guardPos);
       }
 
-      if (!grid.PositionInRange(nextBlock))
+      if (found)
         { break; }
-
-      // We hit a block so rotate right and carry on
-      dir = dir.RotateRight();
     }
 
-    PrintFmt("P1: {}\n", ssize_t(visitedPositions.size()));
+    // Make a bool array that's the same size as our grid initially filled with false (We'll use this to track what's
+    //  visited)
+    auto visits = Array2D<bool>{grid.Width(), grid.Height()};
+    visits.Fill(false);
 
-    // Don't test the initial position for blocking.
-    visitedPositions.erase(initialGuardPos);
+    // Mark the initial position as visited (and increment our visit counter by 1)
+    visits[initialGuardPos] = true;
+    p1++;
 
-    s64 p2 = 0;
-    for (auto t : visitedPositions)
+    auto guardPos = initialGuardPos;
+
+    for (auto dirV = Vec2{0, -1};;)
     {
-      // Temporarily insert this position into the grid
-      blockXsPerY[t.y].insert(std::ranges::upper_bound(blockXsPerY[t.y], t.x), t.x);
-      blockYsPerX[t.x].insert(std::ranges::upper_bound(blockYsPerX[t.x], t.y), t.y);
+      auto n = guardPos + dirV;
+      if (!grid.PositionInRange(n))
+        { break; } // Out of range so we're done.
 
-      // Start at the beginning again
-      guardPos = initialGuardPos;
-      dir = { 0, -1 };
-
-      std::set<std::tuple<Vec2<s32>, Vec2<s32>>> blockAndInitialDir;
-
-      while (true)
+      if (grid[n] == '#')
       {
-        // Find the next place we'll hit (or leave)
-        auto nextBlock = (dir.x != 0)
-          ? Vec2{ FindNextBlock(blockXsPerY[guardPos.y], guardPos.x, dir.x, grid.Width()), guardPos.y }
-          : Vec2{ guardPos.x, FindNextBlock(blockYsPerX[guardPos.x], guardPos.y, dir.y, grid.Height()) };
-
-        // If we're leaving this is not a loop.
-        if (!grid.PositionInRange(nextBlock))
-          { break; }
-
-        // Otherwise we hit a block, add the block and the direction we hit from into the set.
-        auto [_, inserted] = blockAndInitialDir.insert({nextBlock, dir});
-        if (!inserted)
-        {
-          // If it was already in the set then we got here again going the same direction and this is officially a loop
-          p2++;
-          break;
-        }
-
-        // Move to the square before the next block and rotate and keep going.
-        guardPos = nextBlock - dir;
-        dir = dir.RotateRight();
+        // Next spot is a wall so rotate right and carry on.
+        dirV = dirV.RotateRight();
+        continue;
       }
 
-      // Remove our temporary elements.
-      blockXsPerY[t.y].erase(std::ranges::lower_bound(blockXsPerY[t.y], t.x));
-      blockYsPerX[t.x].erase(std::ranges::lower_bound(blockYsPerX[t.x], t.y));
+      // Move and update the visit grid and count if we hadn't already been here.
+      guardPos = n;
+      if (!visits[guardPos])
+      {
+        p1++;
+        visits[guardPos] = true;
+      }
     }
 
+    auto visitDirs = Array2D<DirFlag>{grid.Width(), grid.Height()};
+    constexpr Vec2<s32> dirVecs[] = { { 0, -1 }, { 1, 0 }, { 0, 1 }, { -1, 0 } };
+
+    // We don't want to consider the initial guard post in this.
+    visits[initialGuardPos] = false;
+
+    // I tried a few ways and iterating the visits list and checking for "true"s was the fastest
+    for (auto vy = 0; vy < visits.Height(); vy++)
+    {
+      for (auto vx = 0; vx < visits.Width(); vx++)
+      {
+        auto t = Vec2{vx, vy};
+        if (!visits[t])
+          { continue; } // This isn't a location we visited so there's no point in trying a wall here.
+
+        grid[t] = '#';
+
+        // Start at the beginning
+        guardPos = initialGuardPos;
+        auto dir = Dir::N;
+        auto dirV = dirVecs[s32(dir)];
+
+        visitDirs.Fill(DirFlag::None);
+
+        while (true)
+        {
+          auto n = guardPos + dirV;
+
+          // Move until we go out of range or reach a wall.
+          while (grid.PositionInRange(n) && grid[n] != '#')
+          {
+            guardPos = n;
+            n += dirV;
+          }
+
+          // We're done if we went out of range, this isn't a loop.
+          if (!grid.PositionInRange(n))
+            { break; }
+
+          auto flag = DirFlag(1 << s32(dir));
+          if ((visitDirs[guardPos] & flag) == flag)
+          {
+            // We already reached this block going this direction so this is a loop!
+            p2++;
+            break;
+          }
+
+          // Add our direction to the loop and rotate right.
+          visitDirs[guardPos] |= flag;
+          dir = (dir == Dir::W) ? Dir::N : Dir(s32(dir) + 1);
+          dirV = dirVecs[s32(dir)];
+        }
+
+        // Now remove our temp addition of a wall.
+        grid[t] = '.';
+      }
+    }
+
+    PrintFmt("P1: {}\n", p1);
     PrintFmt("P2: {}\n", p2);
   }
 }

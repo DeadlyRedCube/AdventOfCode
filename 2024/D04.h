dabdba5509b2b1a24557487c83e71549007f004b
Josh Jersild <josh@drilsej.com>
2024-12-06 12:56:08 -0800

    2024 Day 4 improvements
    - Eliminated the Xcoords/Acoords arrays, instead just doing the work
    - Changed the "8 if statements" of part 1 into a loop over all 8 directions as vectors
      - This also means the out of bounds test can just check "st + dir * 3" for in-range
    - Similarly the part 2 test is a loop over two directions (1, 1  and 1, -1) and tests both SAM and MAS in each
    
    It's not much faster but it's much less code

--- 2024/D04.h
+++ 2024/D04.h
@@ -10,108 +10,43 @@ namespace D04
 
     auto grid = ReadFileAsCharArray(path);
 
-    UnboundedArray<Vec2<s32>> Xcoords;
-    UnboundedArray<Vec2<s32>> Acoords;
     for (s32 y = 0; y < grid.Height(); y++)
     {
       for (s32 x = 0; x < grid.Width(); x++)
       {
+        auto st = Vec2{x, y};
         if (grid[x, y] == 'X')
-          { Xcoords.Append({x, y}); }
+        {
+          for (auto dir
+            : { Vec2{1, 0}, Vec2{0, 1}, Vec2{-1, 0}, Vec2{0, -1}, Vec2{1, 1}, Vec2{1, -1}, Vec2{-1, 1}, Vec2{-1, -1} })
+          {
+            // If the end of this test is out of bounds we can skip it.
+            if (!grid.PositionInRange(st + dir * 3))
+              { continue; }
+            if (grid[st + dir] == 'M' && grid[st + dir * 2] == 'A' && grid[st + dir * 3] == 'S')
+              { p1++; }
+          }
+        }
 
         // Find any 'A's that are in one from the edge.
-        if (grid[x, y] == 'A' && x > 0 && y > 0 && x < grid.Width() - 1 && y < grid.Width() - 1)
-          { Acoords.Append({x, y}); }
+        if (grid[x, y] == 'A' && x > 0 && y > 0 && x < grid.Width() - 1 && y < grid.Height() - 1)
+        {
+          s32 masCount = 0;
+          for (auto dir : { Vec2{1, 1}, Vec2{1, -1} })
+          {
+            if ((grid[st - dir] == 'M' && grid[st + dir] == 'S')
+              || (grid[st - dir] == 'S' && grid[st + dir] == 'M'))
+              { masCount++; }
+          }
+
+          ASSERT(masCount <= 2);
+          if (masCount == 2)
+            { p2++; }
+        }
       }
     }
 
-    // fwd Horizontal search
-    for (auto st : Xcoords)
-    {
-      ASSERT(grid[st] == 'X');
-
-      // Cardinal
-      if (st.x <= grid.Width() - 4
-        && grid[st.x + 1, st.y] == 'M'
-        && grid[st.x + 2, st.y] == 'A'
-        && grid[st.x + 3, st.y] == 'S')
-        { p1++; }
-
-      if (st.x >= 3
-        && grid[st.x - 1, st.y] == 'M'
-        && grid[st.x - 2, st.y] == 'A'
-        && grid[st.x - 3, st.y] == 'S')
-        { p1++; }
-
-      if (st.y <= grid.Height() - 4
-        && grid[st.x, st.y + 1] == 'M'
-        && grid[st.x, st.y + 2] == 'A'
-        && grid[st.x, st.y + 3] == 'S')
-        { p1++; }
-
-      if (st.y >= 3
-        && grid[st.x, st.y - 1] == 'M'
-        && grid[st.x, st.y - 2] == 'A'
-        && grid[st.x, st.y - 3] == 'S')
-        { p1++; }
-
-      // Diagonal
-      if (st.x <= grid.Width() - 4
-        && st.y <= grid.Height() - 4
-        && grid[st.x + 1, st.y + 1] == 'M'
-        && grid[st.x + 2, st.y + 2] == 'A'
-        && grid[st.x + 3, st.y + 3] == 'S')
-        { p1++; }
-
-      if (st.x >= 3
-        && st.y >= 3
-        && grid[st.x - 1, st.y - 1] == 'M'
-        && grid[st.x - 2, st.y - 2] == 'A'
-        && grid[st.x - 3, st.y - 3] == 'S')
-        { p1++; }
-
-      if (st.x <= grid.Width() - 4
-        && st.y >= 3
-        && grid[st.x + 1, st.y - 1] == 'M'
-        && grid[st.x + 2, st.y - 2] == 'A'
-        && grid[st.x + 3, st.y - 3] == 'S')
-        { p1++; }
-
-      if (st.x >= 3
-        && st.y <= grid.Height() - 4
-        && grid[st.x - 1, st.y + 1] == 'M'
-        && grid[st.x - 2, st.y + 2] == 'A'
-        && grid[st.x - 3, st.y + 3] == 'S')
-        { p1++; }
-    }
-
     PrintFmt("P1: {}\n", p1);
-
-    for (auto st : Acoords)
-    {
-      s32 masCount = 0;
-      // Search NW to SE
-      if (grid[st.x - 1, st.y - 1] == 'M'
-        && grid[st.x + 1, st.y + 1] == 'S')
-        { masCount++; }
-
-      if (grid[st.x - 1, st.y - 1] == 'S'
-        && grid[st.x + 1, st.y + 1] == 'M')
-        { masCount++; }
-
-      // Search NE to SW
-      if (grid[st.x - 1, st.y + 1] == 'M'
-        && grid[st.x + 1, st.y - 1] == 'S')
-        { masCount++; }
-
-      if (grid[st.x - 1, st.y + 1] == 'S'
-        && grid[st.x + 1, st.y -  1] == 'M')
-        { masCount++; }
-
-      ASSERT(masCount <= 2);
-      if (masCount == 2)
-        { p2++; }
-    }
 
     PrintFmt("P2: {}\n", p2);
   }

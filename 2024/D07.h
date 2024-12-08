13bfa10f4ff7008367a731ab89ff4746a59fb5b9
Josh Jersild <josh@drilsej.com>
2024-12-07 19:47:28 -0800

    2024 Day 7 Even More optimization (2.3ms now)
    Realized that if you did the operations from right to left (Basically undoing the operations as you go) you can early out faster (especially on multiply/concatenation) because you can check the modulo of the operation and immediately fail if  it's not a proper multiple.
    
    That's probably good now. Way faster than the initial time.

--- 2024/D07.h
+++ 2024/D07.h
@@ -3,36 +3,45 @@
 
 namespace D07
 {
-  // Functions for the ops to pass to the recurse function as template parameters.
-  s64 Add(s64 l, s64 r)
-    { return l + r; }
+  // Subtract r from l if it's greater (otherwise return nullopt, signaling "this operation failed")
+  std::optional<s64> UnAdd(s64 l, s64 r)
+    { return (l > r) ? std::optional{l - r} : std::nullopt; }
 
-  s64 Mul(s64 l, s64 r)
-    { return l * r; }
+  // Divide r from l assuming l is a multiple of r (if it's not a multiple, return nullopt)
+  std::optional<s64> UnMul(s64 l, s64 r)
+    { return ((l % r) == 0) ? std::optional{l / r} : std::nullopt; }
 
-  s64 Concat(s64 l, s64 r)
+  // unconcatenate the digits of R from L (and return nullopt if it couldn't be done)
+  std::optional<s64> UnConcat(s64 l, s64 r)
   {
-    // Multiply l by 10 for every base-10 digit that r contains before summing them (effectively concatenating the digits)
+    // Calculate pow(10, Base10DigitCount(r))
+    s64 tens = 1;
     for (auto d = r; d > 0; d /= 10)
-      { l *= 10; }
-    return l + r;
+      { tens *= 10; }
+
+    // the modulo of l and the tens value should be the number on the right (think if you concatenated 123 with 45, the
+    //  "tens" value would be 100 and 12345 % 100 == 45), if so, do the divide by tens to "remove" the right value's
+    //  digits (int multiplication does a floor so this works great), otherwise we can't and return nullopt.
+    return (l % tens == r) ? std::optional{l / tens} : std::nullopt;
   }
 
-  using Func = s64(*)(s64, s64);
-  template <Func ...funcs>
-  bool Recurse(const std::vector<s64> &v, u32 begin, s64 result)
+  using UnFunc = std::optional<s64>(*)(s64, s64);
+  template <UnFunc ...funcs>
+  bool UnRecurse(const std::vector<s64> &v, s64 result, u32 begin)
   {
-    // If we're at the end test to see if our previous result matches the test value.
-    if (begin == v.size())
-      { return result == v[0]; }
+    // If we're at the end test to see if our result matches the first item in the list, which means if we'd done the
+    //  operations from left to right they would have matched.
+    if (begin == 1)
+      { return result == v[1]; }
 
-    // If we're already larger than the goal we can quit early.
-    if (result > v[0])
-      { return false; }
-
-    // Run the recurse function for every func template parameter as an || operator which will nicely early-out if
-    //  an earlier one succeeds.
-    return ((Recurse<funcs...>(v, begin + 1, funcs(result, v[begin]))) || ...);
+    return (... || // Use a fold expression here to run the following lambda for every element of the funcs pack
+      [&]
+      {
+        // Calculate the result of undoing the operation at this point. If it has no value we're done (op couldn't be
+        //  undone in a valid way), otherwise recurse into this function using the previous value in the list.
+        auto opt = funcs(result, v[begin]);
+        return opt.has_value() ? UnRecurse<funcs...>(v, *opt, begin - 1) : false;
+      }());
   }
 
 
@@ -44,17 +53,17 @@ namespace D07
     for (auto line : ReadFileLines(path))
     {
       auto vals = Split(line, " :", KeepEmpty::No) | std::views::transform(AsS64) | std::ranges::to<std::vector>();
-      if (Recurse<Add, Mul>(vals, 2, vals[1]))
+
+      // If the P1 test works (using only add and mul) then p2 is also valid and we don't have to do the heavier test.
+      if (UnRecurse<UnAdd, UnMul>(vals, vals[0], u32(vals.size() - 1)))
       {
-        // If it's good for part 1 it's good for both parts
         p1 += vals[0];
         p2 += vals[0];
         continue;
       }
 
-      // Try again with all of the ops enabled for P2 only. I thought it might run faster to put Concat first but
-      //  instead it's slightly slower (I think because it's the more complex op and frontloading it does more work)
-      if (Recurse<Add, Mul, Concat>(vals, 2, vals[1]))
+      // Try again with all of the ops enabled for P2
+      if (UnRecurse<UnConcat, UnAdd, UnMul>(vals, vals[0], u32(vals.size() - 1)))
         { p2 += vals[0]; }
     }
 

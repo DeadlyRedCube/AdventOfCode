d8b31287645b4c7c88f21c2ac13781a54b2e477a
Josh Jersild <josh@drilsej.com>
2024-12-21 22:51:09 -0800

    2024 Day 22 Optimized (Runs in 16.39ms)
    Reworked the map to be an array instead (overallocating the space to make it easier to index), putting the "have we seen this one" index into the array directly, removing an unnecessary mask, and factoring an extraneous modulo out.
    
    Yay

--- 2024/D22.h
+++ 2024/D22.h
@@ -3,61 +3,82 @@
 
 namespace D22
 {
-  using Vec = Vec2<s32>;
-  constexpr auto X = Vec::XAxis();
-  constexpr auto Y = Vec::YAxis();
+  // An array of possibilities (the lookup index is the four previous changes (p0-p3) where it's
+  //  Sum(n == 0..3) of (pn + 9) << ((3 - n) * 5)
+  //  That is to say, add 9 to ensure a positive value (these changes are using bets mod 10 so only -9 to 9 are valid)
+  //   then shifted up 5 per age
+  //  (this means there are array entries that never get used because there's 19 possible values per change but we're
+  //   shifting by 32, but that's purely so that it can be shifts and masking instead of muls and modulos).
+  constexpr u32 PossibilityKeyCount = 1024*1024;
+
+  struct Possibility
+  {
+    s16 bananaCount;    // The number of bananas seen for this possibility
+    s16 lastSeenIndex;  // The last index of a secret number that updated the banana count.
+  };
+
+  std::array<Possibility, PossibilityKeyCount> possibilities;
 
   void Run(const char *filePath)
   {
     s64 p1 = 0;
-    s64 p2 = 0;
+    s16 p2 = 0;
 
-    auto lines = ReadFileLines(filePath);
-
-    std::map<std::array<s64, 4>, s64> changes;
-
-    for (auto &line : lines)
+    for (auto [secretNumIndex, secretNum]
+      : std::views::enumerate( ReadFileLines(filePath) | std::views::transform(AsS32)))
     {
-      auto secretNum = AsS64(line);
+      u32 possibilityKey = 0;
 
-      std::array<s64, 4> prevs;
-      std::set<std::array<s64, 4>> set;
-      for (u32 i = 0; i < 4; i++)
-      {
-        s64 orig = secretNum;
-        secretNum = (secretNum ^ (secretNum * 64)) % 16777216;
-        secretNum = (secretNum ^ (secretNum / 32)) % 16777216;
-        secretNum = (secretNum ^ (secretNum * 2048)) % 16777216;
-        prevs[i] = (secretNum % 10) - (orig % 10);
-      }
+      // Store the bet before this one (to compute the changes)
+      u32 prevBet = secretNum % 10;
 
-      changes[prevs] += (secretNum % 10);
-      set.insert(prevs);
-
-      for (u32 i = 4; i < 2000; i++)
-      {
-        for (u32 j = 0; j < 3; j++)
-          { prevs[j] = prevs[j + 1]; }
-
-        s64 orig = secretNum;
-        secretNum = (secretNum ^ (secretNum * 64)) % 16777216;
-        secretNum = (secretNum ^ (secretNum / 32)) % 16777216;
-        secretNum = (secretNum ^ (secretNum * 2048)) % 16777216;
-        prevs[3] = (secretNum % 10) - (orig % 10);
-
-        if (!set.contains(prevs))
+      // Made this a lambda to not repeat the logic.
+      auto DoHashAndGetCurrentBet =
+        [&]
         {
-          changes[prevs] += (secretNum % 10);
-          set.insert(prevs);
+          // Do the hash
+          secretNum ^= (secretNum << 6) & 0xffffff;
+          secretNum ^= (secretNum >>  5);
+          secretNum ^= (secretNum << 11) & 0xffffff;
+
+          // Calculate the bet (lowest digit)
+          auto curBet = (secretNum % 10);
+
+          // Shift the key over 5 (moving things earlier in the history) then mix in the change (+ 9 so it's positive)
+          //  (then mask off the upper bits for values that are too old)
+          possibilityKey = ((possibilityKey << 5) | (curBet - prevBet + 9)) & (PossibilityKeyCount - 1);
+          prevBet = curBet;
+
+          return s16(curBet);
+        };
+
+      // Do 3 hash values without checking possibilities (since there are no sets of four changes until, you know,
+      //  the fourth price change)
+      for (u32 i = 0; i < 3; i++)
+        { (void)DoHashAndGetCurrentBet(); }
+
+      // Now do all the rest
+      for (u32 i = 3; i < 2000; i++)
+      {
+        auto curBet = DoHashAndGetCurrentBet(); // This time we care about the current bet
+
+        auto &p = possibilities[possibilityKey];
+
+        // Check to see if this secret number already updated this possibility yet.
+        if (p.lastSeenIndex != secretNumIndex)
+        {
+          // ...we hadn't updated this one yet, so update it now.
+          p.bananaCount += curBet;
+          p.lastSeenIndex = s16(secretNumIndex);
+
+          // Update the maximum value we've found thus far.
+          p2 = std::max(p2, p.bananaCount);
         }
       }
 
+      // For part 1 we just care about the sums of all of the secret numbers
       p1 += secretNum;
     }
-
-
-    for (auto &&[k, v] : changes)
-      { p2 = std::max(p2, v); }
 
     PrintFmt("P1: {}\n", p1);
     PrintFmt("P2: {}\n", p2);
